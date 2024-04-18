05d6b2102cb3a3c885f6afe0384237933367c955
Mohamed Yahia <m.yahya199290@gmail.com>
2024-04-18 13:55:36 +0300

    created admin navbar

--- app/admin/_components/header/header.jsx
+++ app/admin/_components/header/header.jsx
@@ -5,15 +5,15 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="admin-head">
-        <h4 className="logo">
+    <header className="flex align-middle justify-between bg-purple-100 p-6">
+        <h4 className="text-2xl font-bold uppercase">
             myh shop
         </h4>
-        <nav>
-            <Link href={`/admin/dashbourd`}>dashbourd</Link>
-            <Link href={`/admin/products`}>products</Link>
-            <Link href={`/admin/customers`}>customers</Link>
-            <Link href={`/admin/customers`}>sales</Link>
+        <nav className="flex align-middle gap-5">
+            <Link href={`/admin/dashbourd`} className="capitalize text-xl text-black-100 hover:text-black-500">dashbourd</Link>
+            <Link href={`/admin/products`} className="capitalize text-xl text-black-100 hover:text-black-500">products</Link>
+            <Link href={`/admin/customers`} className="capitalize text-xl text-black-100 hover:text-black-500">customers</Link>
+            <Link href={`/admin/customers`} className="capitalize text-xl text-black-100 hover:text-black-500">sales</Link>
         </nav>
     </header>
   )
