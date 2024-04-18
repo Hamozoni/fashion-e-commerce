4fbf465205e8dd4b53fc847f209c0c00d92765fe
Mohamed Yahia <m.yahya199290@gmail.com>
2024-04-18 21:18:23 +0300

    created new next app becouse can not run

--- app/admin/products/add_new_product/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AddNewProduct = () => {
-  return (
-    <div>new add</div>
-  )
-}
-
-export default AddNewProduct
--- my-app/app/admin/products/page.jsx
+++ my-app/app/admin/products/page.jsx
@@ -1,13 +1,13 @@
 "use client"
 import Link from "next/link"
-import AddIcon from '@mui/icons-material/Add';
+// import AddIcon from '@mui/icons-material/Add';
 
 const Products = () => {
   return (
     <div className="">
         <Link href='/admin/products/add_new_product' className="flex" >
             <span>add new product</span>
-            <AddIcon />
+            {/* <AddIcon /> */}
         </Link >
     </div>
   )
