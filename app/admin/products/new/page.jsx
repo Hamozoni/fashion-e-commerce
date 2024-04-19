24e10810bd4f37abccb9aa93ef78edb98af92a31
Mohamed Yahia <m.yahya199290@gmail.com>
2024-04-19 21:58:21 +0300

    modified:   app/admin/products/new/page.jsx

--- app/admin/products/new/page.jsx
+++ app/admin/products/new/page.jsx
@@ -12,49 +12,114 @@ const NewProducts = () => {
   return (
     <div className="p-4 lg:p-10 w-full">
         <form className="mx-auto  border border-slate-100 p-4 rounded-md   min-w-fit shadow-lg" action="" type='POST' >
-        <h3 className="py-4 mb-4 font-bold text-2xl border-b border-slate-100 ">adding new product form</h3>
+        <h3 className="py-4 mb-6 font-bold text-2xl border-b border-slate-100 ">adding new product form</h3>
             <div className={className.inputsDev}>
                 <label className={className.label}  htmlFor="title">title : </label>
-                <input type="text" name="title" id="title" className={className.inputClass} placeholder="the name of the product..." />
+                <input 
+                    type="text" 
+                    name="title" 
+                    id="title" 
+                    className={className.inputClass} 
+                    placeholder="the name of the product..."
+                    required
+                     />
             </div>
             <div className={className.inputsDev}>
                 <label className={className.label}  htmlFor="price">price : </label>
-                <input className={className.inputClass} type="number" name="price" id="price" placeholder="the price of the product in cent..." />
-            </div>
-            <div className={className.inputsDev}>
-                <label className={className.label}  htmlFor="category">category : </label>
-                <input className={className.inputClass} type="text" name="category" id="category" placeholder="the category of the product..." />
+                <input 
+                    className={className.inputClass} 
+                    type="number" 
+                    name="price" 
+                    id="price" 
+                    placeholder="the price of the product in cent..."
+                    required
+                     />
             </div>
             <div className={className.inputsDev}>
                 <label className={className.label}  htmlFor="description">description : </label>
-                <textarea className={className.inputClass}  name="description" id="description" placeholder="the description of the product..." cols="10" rows="10"></textarea>
+                <textarea 
+                    className={className.inputClass}  
+                    name="description" 
+                    id="description" 
+                    placeholder="the description of the product..." 
+                    cols="10" 
+                    rows="10"
+                    required
+                    ></textarea>
+            </div>
+            <div className={className.inputsDev}>
+                <label className={className.label}  htmlFor="category">category : </label>
+                <input 
+                    className={className.inputClass} 
+                    type="text" 
+                    name="category" 
+                    id="category" 
+                    placeholder="the category of the product..."
+                     />
             </div>
             <div className={className}>
                 <label  className={className.label} htmlFor="subcategory">subcategory : </label>
-                <input className={className.inputClass} type="text" name="subcategory" id="subcategory" placeholder="the subcategory of the product..." />
+                <input 
+                    className={className.inputClass} 
+                    type="text" 
+                    name="subcategory" 
+                    id="subcategory" 
+                    placeholder="the subcategory of the product..."
+                     />
             </div>
             <div className={className}>
-                <label className={className.label}  htmlFor="count">count : </label>
-                <input className={className.inputClass} type="number" name="count" id="count" placeholder="the count of the product..." />
+                <label className={className.label}  htmlFor="about">about this item : </label>
+               <textarea 
+                    className={className.inputClass}  
+                    name="about" 
+                    id="about" 
+                    placeholder="about this item" 
+                    cols="10" rows="10"
+                    required
+                    ></textarea>
             </div>
             <div className={className}>
                 <label className={className.label}  htmlFor="specifications">specifications : </label>
-               <textarea className={className.inputClass}  name="specifications" id="specifications" placeholder="specifications should be key_value; key_value;key_value;else.." cols="10" rows="10"></textarea>
+               <textarea 
+                    className={className.inputClass}  
+                    name="specifications" 
+                    id="specifications" 
+                    placeholder="specifications should be key_ _value|| key_ _value|| key_ _value||else.." 
+                    cols="10" rows="10"
+                    required
+                    ></textarea>
                
             </div>
             <div className={`${className.inputsDev} flex gap-4`}>
                 <div className='{className}'>
                     <label className={className.label}  htmlFor="not-availble"> not availble : </label>
-                    <input type="radio" name="notAvailble" id="not-availble" />
+                    <input type="radio" name="availble" value='no' id="not-availble" />
                 </div>
                 <div className='{className}'>
                    <label className={className.label}  htmlFor="availble">availble : </label>
-                   <input type="radio" name="availble" id="availble" />
+                   <input type="radio" name="availble" value='yes' id="availble" />
                 </div>
+            </div>
+            <div className={className}>
+                <label className={className.label}  htmlFor="count">count : </label>
+                <input 
+                    className={className.inputClass} 
+                    type="number" 
+                    name="count" 
+                    id="count" 
+                    placeholder="the count of the product..."
+                    required
+                     />
             </div>
             <div className={className.inputsDev}>
                 <label className={className.label}  htmlFor="image-path">image : </label>
-                <input className={className.inputClass} type="file" name="imagePath" id="image-path" />
+                <input 
+                    className={className.inputClass} 
+                    type="file" 
+                    name="imagePath" 
+                    id="image-path" 
+                    required
+                    />
             </div>
 
             <button type="submit" className={className.sumBtn}>save</button>
