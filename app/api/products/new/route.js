449badfe811a051c67be9e294f4b45911ce78faa
Mohamed Yahia <m.yahya199290@gmail.com>
2024-04-24 12:51:51 +0300

    modified:   app/api/products/new/route.js

--- app/api/products/new/route.js
+++ app/api/products/new/route.js
@@ -1,12 +1,30 @@
-import { headers } from "next/headers"
+import fs  from 'fs/promises';
+
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
 
 export async function POST(request) {
 
-    const h = headers();
+    const data = await request.json();
 
-    const data = await request.json()
 
-    console.log(data)
-    console.log(h)
+    await prisma.product.create({data : data.products});
+    await prisma.specifications.createMany({data : data.specifications});
+    await prisma.productSizes.createMany({data : data.sizes});
+
+    await fs.mkdir("pubilc/products",{recursive: true})
+
+    for(let i = 0; i < data.images.length; i++){
+        const imgPath = `pubilc/products/${crypto.randomUUID()}-${data.images[i].imagePath}`;
+
+        await fs.writeFile(imgPath,Buffer.from(await data.images[i].imagePath.arrayBuffer()))
+
+        data.images[i].imagePath = imgPath
+    };
+
+    await prisma.productImages.createMany({data : data.images});
+
   return new Response('hi')
 }
