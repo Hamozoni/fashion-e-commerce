4c8c8084723159173e4378246126b99eeab61edf
Mohamed Yahia <m.yahya199290@gmail.com>
2024-05-02 23:24:29 +0300

    app/_components/Categories.jsx

---
+++ app/_components/Categories.jsx
@@ -0,0 +1,19 @@
+
+
+function Categories() {
+  return (
+     <section className="flex  items-center gap-4">
+          <div className="">
+               <button>men</button>
+          </div>
+          <div className="">
+               <button>women</button>
+          </div>
+          <div className="">
+               <button>kids</button>
+          </div>
+     </section>
+  )
+}
+
+export default Categories
