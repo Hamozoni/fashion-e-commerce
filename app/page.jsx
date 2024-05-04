dbe6c98f5622c8a8819c0a5758ff00a426bf0ec0
Mohamed Yahia <m.yahya199290@gmail.com>
2024-05-05 00:07:10 +0300

    created modified:   app/_ui/home/slider.jsx         modified:   app/page.jsx         deleted:    public/slide/slide2.avif

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,10 @@
 import CategoriesCards from "./_ui/components/categoriesCards";
+import Slide from "./_ui/home/slider";
 
 export default function Home() {
   return (
     <div className="">
+      <Slide />
        <CategoriesCards/>
     </div>
   );
