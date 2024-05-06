17ebbe651af8c5651566d42c5a1c4364cc1d9b84
Mohamed Yahia <m.yahya199290@gmail.com>
2024-05-07 00:00:46 +0300

    modified:   app/api/product/route.js

--- app/api/product/route.js
+++ app/api/product/route.js
@@ -3,7 +3,7 @@ import {prisma} from '../../_lip/db'
 export async function GET (_reg) {
 
     const data  = await prisma.product.findMany(
-        {include: {images:true,{images:true} }})
+        {include: {images:true,images:true }})
     console.log("results",data)
 
     return new Response(JSON.stringify(data))
