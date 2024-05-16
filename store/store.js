b5065a274c370c1df66cdb1b3ac32a7b892f7c45
Mohamed Yahia <m.yahya199290@gmail.com>
2024-05-16 20:08:44 +0300

    created redux store

---
+++ store/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
+
+export const store = configureStore(
+    {
+        reducer: {}
+    }
+);
+
+export const useAppDispatch = useDispatch;
+export const useAppSelector = useSelector;
