5ce3984a6d1233c61f537bf929316b10261cdd21
Mohamed Yahia <m.yahya199290@gmail.com>
2024-05-16 22:29:01 +0300

    modified:   store/features/cartSlice.js

---
+++ store/features/cartSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    cartItems :[]
+}
+
+export const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        
+    }
+})
