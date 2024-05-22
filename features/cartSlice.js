ed404b6944429e79daaeca752e5f30b09a896bf0
Mohamed Yahia <m.yahya199290@gmail.com>
2024-05-22 09:46:03 +0300

    creating cart slice reducer

--- features/cartSlice.js
+++ features/cartSlice.js
@@ -12,9 +12,24 @@ export const cartSlice = createSlice({
 
             const item = state.cartItems.find(el=> el.product.id === action.payload.id);
 
-            if(item) item.q++
+            if(item) item.q++;
+            else state.cartItems.product.push({product: action.payload,q: 1})
             
+            
+        },
+        decrement: (state,action)=> {
+            const item = state.cartItems.find(el=> el.product.id === action.payload.id);
+            if(item){
+                item.q--
+                if(item.q === 0){
+                    state.cartItems = state.cartItems.filter(e=> e.product.id !== action.payload.id)
+                }
+            }
         }
         
     }
-})
+});
+
+export const {increment,decrement} = cartSlice.actions;
+
+export default cartSlice.reducer;
