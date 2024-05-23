c9e4835f3f97e67d0055d7905345f350bea766c0
Mohamed Yahia <m.yahya199290@gmail.com>
2024-05-23 20:32:24 +0300

    modified:   app/cart/_components/CartItemsCard.jsx         modified:   app/cart/page.jsx

--- app/cart/_components/CartItemsCard.jsx
+++ app/cart/_components/CartItemsCard.jsx
@@ -1,16 +1,26 @@
-import Image from "next/image"
+import Image from "next/image";
+import getCurrency from "../../_lip/getCurrency";
 
 function CartItemsCard({product}) {
+
+  const className = {
+
+  }
   return (
-    <div>
+    <div className="flex gap-3 p-3 border-b border-green-100">
         <div className="">
-            <Image src={product.img.replace("public","")} />
+            <Image 
+                src={product?.product?.images[0]?.imagePath?.replace("public","")}
+                width={150}
+                height={150}
+                alt='product image'
+                 />
         </div>
         <div className="">
-            <h4>{product.product.name}</h4>
+            <h4>{product?.product?.name}</h4>
             <h6> color: 
                 <span 
-                    style={{background: product.color}}
+                    style={{background: product?.color}}
                     className="w-[35px] h-[35px] rounded-full"
                     >
                 </span>
@@ -19,12 +29,22 @@ function CartItemsCard({product}) {
                 <span 
                     className="w-[35px] h-[35px] rounded-full"
                     >
-                        {product.size}
+                        {product?.size}
                 </span>
             </h6>
         </div>
-        <div className=""></div>
-        <div className=""></div>
+        <div className="">
+            <h6>price</h6>
+            <h4>{getCurrency(product?.product?.priceInCent)}</h4>
+        </div>
+        <div className="">
+            <h6>quantity</h6>
+            <h4>{product?.quantity}</h4>
+        </div>
+        <div className="">
+            <h6>total price</h6>
+            <h4>{getCurrency(product?.product?.priceInCent * product?.quantity)}</h4>
+        </div>
     </div>
   )
 }
--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -3,20 +3,23 @@
 import Image from "next/image";
 import { useAppSelector } from "../../store/store";
 import Link from "next/link";
+import CartItemsCard from "./_components/CartItemsCard"
 
 function page() {
 
     const cartItems = useAppSelector(state=> state.cart.cartItems);
 
-    console.log(cartItems?.length)
+    console.log(cartItems)
     const className = {
         startShopping: 'border border-green-200 px-4 py-1 text-green-900 uppercase bg-green-100 hover:bg-green-50 rounded-md absolute bottom-[20px] left-[50%] translate-x-[-50%]'
     }
 
+ const cartCard = cartItems?.map((product)=> <CartItemsCard product={product}/>)
+
   return (
     <div className="p-4 lg:px-8">
         {
-            cartItems?.length ? "":
+            cartItems?.length ? cartCard :
             <div className="relative max-w-fit mx-auto">
                 <Image src='/cart/emtyCart.png' width={404}  height={316} alt='emty cart'/>
                 <Link className={className.startShopping} href='/' >
