8732c697919a9e2a8defc57e962f4b6096563fb6
Mohamed Yahia <m.yahya199290@gmail.com>
2024-05-24 13:19:32 +0300

    creatng cart checkout component

--- app/cart/_components/CartSummary.jsx
+++ app/cart/_components/CartSummary.jsx
@@ -26,36 +26,45 @@ function CartSummary() {
          <li key={img}>
             <Image  src={img} width={50} height={50} alt="payment"/>
         </li>
-    ))
+    ));
+
+    const className = {
+        section : 'p-3 border border-gray-100 mb-3 rounded-md',
+        parts: 'flex items-center justify-between py-2 border-b border-gray-50',
+        checkoutBtn: 'w-full rounded-md py-2 bg-green-900 uppercase text-green-50 hover:bg-green-800'
+    }
 
 
   return (
     <div className="flex-1">
-        <header>
-            <div className="">
-                <MdOutlineLocalShipping />
-                <h4>free shipping</h4>
-                <p>on orders over SAR 150 </p>
+        <header className={className.section}>
+            <div className="flex items-center gap-2">
+                <MdOutlineLocalShipping size={22}/>
+                <h4 className="uppercase text-green-900">free shipping</h4>
+                <p className="text-sm text-green-800">on orders over <strong > SAR 150 </strong></p>
             </div>
         </header>
-        <div className="flex items-center justify-between">
-            <h5>{`subtotal (${subtotal} items):`}</h5>
-            <h5> {totalPrice} </h5>
+        <div className={className.section}>
+            <div className={className.parts}>
+                <h5>{`subtotal (${subtotal} items):`}</h5>
+                <h5> {getCurrency(totalPrice)} </h5>
+            </div>
+            <div className={className.parts}>
+                <h5>shipping fee</h5>
+                <h5> {deliveryFree > 0 ? getCurrency(deliveryFree): "free"} </h5>
+            </div>
+            <div className={className.parts}>
+                <h5>total: </h5>
+                <h5> {getCurrency(totalPrice + deliveryFree)} </h5>
+            </div>
+            <button className={className.checkoutBtn}>
+                checkout
+            </button>
         </div>
-        <div className="flex items-center justify-between">
-            <h5>shipping fee</h5>
-            <h5> {deliveryFree > 0 ? getCurrency(deliveryFree): "free"} </h5>
-        </div>
-        <div className="flex items-center justify-between">
-            <h5>total: </h5>
-            <h5> {getCurrency(totalPrice + deliveryFree)} </h5>
-        </div>
-        <button className="w-full rounded-md py-2 bg-green-200 uppercase text-green-900 hover:bg-green-100">
-             checkout
-        </button>
-        <section>
+
+        <section className={className.section}>
             <h5>ways you can pay :</h5>
-            <ul className="flex items-center gap-2 flex-wrap">
+            <ul className="flex items-center gap-2 flex-wrap justify-center">
                 {payment}
             </ul>
         </section>
