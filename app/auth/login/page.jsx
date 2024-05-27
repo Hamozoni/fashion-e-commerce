a370db43bb0894386db79d60dc0a7645d55accf2
Mohamed Yahia <m.yahya199290@gmail.com>
2024-05-27 18:50:18 +0300

    modified:   app/auth/login/page.jsx

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -1,14 +1,25 @@
 import AuthHeader from "../_components/authHeader"
-
-
+import { AuthInput } from "../_components/authInput"
+import { loginInputs } from "../_components/authInputsData"
+import AuthSocial from "../_components/authSocial"
 function LoginPage() {
   return (
     <div>
         <div className="">
             <AuthHeader text='login'/>
             <form action="">
-                
+                {
+                    loginInputs?.map(input=> (
+                        <AuthInput 
+                            key={input.name} 
+                            type={input.type} 
+                            name={input.name} 
+                            Icon={input.Icon}
+                            />
+                    ))
+                }
             </form>
+            <AuthSocial text='don not have an account' link='/auth/register' />
         </div>
     </div>
   )
