5ae893aff35e52701bc46eab0a1327d7fd149b04
Mohamed Yahia <m.yahya199290@gmail.com>
2024-05-30 09:07:21 +0300

    modified:   validationSchemas/authSchemas.js

--- validationSchemas/authSchemas.js
+++ validationSchemas/authSchemas.js
@@ -13,11 +13,9 @@ export const registerSchema = z.object({
 });
 
 export const loginSchema = z.object({
-    name : z.string().min(3),
     email : z
     .string()
     .min(1, { message: "This field has to be filled." })
     .email("This is not a valid email."),
     password: z.string().min(6),
-    confirm_password: z.string().min(6),
 })
