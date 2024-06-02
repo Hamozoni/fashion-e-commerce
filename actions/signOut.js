fa418cb3b7871d55bdb3299f918f72ba01230030
Mohamed Yahia <m.yahya199290@gmail.com>
2024-06-02 18:02:36 +0300

    actions/signOut.js

---
+++ actions/signOut.js
@@ -0,0 +1,9 @@
+"use server";
+
+import {signOut} from "../auth";
+
+export const signOutAction = async()=>{
+
+    await signOut()
+
+}
--- components/MainHeader.jsx
+++ components/MainHeader.jsx
@@ -1,16 +1,30 @@
+"use client";
 
 import Link from "next/link";
 import HeaderUserBar from "./HeaderUserBar";
 import Categories from "./Categories";
 import SearchBar from "./SearchBar";
-import { signOut } from "next-auth/react";
+import { signOutAction } from "../actions/signOut";
+import { useTransition } from "react";
+import Overlay from "./Overlay";
+import {SyncLoader} from "react-spinners";
 
 
 async function MainHeader () {
 
+    const [isLoading,startTranation] = useTransition()
+
     const className = {
         flex:  `flex items-center gap-8`,
         logo: `uppercase text-2xl font-extrabold text-lime-700`
+    }
+
+    const signOut = ()=> {
+
+        startTranation(()=> {
+            signOutAction();
+        })
+
     }
   return (
     <header className="bg-lime-100 sticky top-0 left-0 w-full z-50 py-2 px-4 lg:px-8">
@@ -25,15 +39,21 @@ async function MainHeader () {
                 <Categories/>
                 <HeaderUserBar />
             </section>
-            <form action={async()=> {
-                   "use server"
-                    await signOut()
-                }}
+            <form action={signOut}
             >
                <button >sign out</button>
 
             </form>
         </div>
+        {
+            isLoading && 
+            <>
+              <Overlay />
+              <div className="fixed top-0 left-0 w-[100vw] h-[100vh] flex justify-center items-center z-50">
+                  <SyncLoader size={30} />
+              </div>
+            </>
+        }
     </header>
   )
 }
