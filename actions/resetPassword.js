b22d93d65848ccf5f1b1b4f2c7a116648c95bd82
Mohamed Yahia <m.yahya199290@gmail.com>
2024-06-04 16:53:18 +0300

    actions/resetPassword.js

---
+++ actions/resetPassword.js
@@ -0,0 +1,9 @@
+
+
+export async function resetPasswordAction(email) {
+
+
+
+}
+
+ 
--- app/auth/reset-password/page.jsx
+++ app/auth/reset-password/page.jsx
@@ -1,9 +1,44 @@
+"use client";
 
+import { useRef, useTransition } from "react";
+import { AuthInput } from "../_components/authInput";
+
+import { MdOutlineMail } from "react-icons/md";
+import {resetPasswordAction} from "../../../actions/resetPassword";
+import { SubmitBtn } from "../_components/submitBtn";
+import AuthHeader from "../_components/authHeader";
 
 const ResetPassword = ()=> {
+
+    const [isLoading,startTranation] = useTransition();
+    const resetForm = useRef()
+
+    const reset = ()=> {
+
+        const formData = new FormData(resetForm.current);
+
+        startTranation(()=> {
+            resetPasswordAction(formData)
+        })
+
+    };
+
+
     return (
-        <div className="">
-            reset
+        <div className=" bg-white w-[450px] p-4 rounded-md shadow-md">
+            <AuthHeader text='reset password' />
+            <form ref={resetForm} action={reset}>
+                <AuthInput 
+                    key='' 
+                    type='email' 
+                    name='email'
+                    Icon={MdOutlineMail}
+                    isLoading={isLoading}
+                    />
+
+                    <SubmitBtn isLoading={isLoading} text='submit' />
+
+            </form>
         </div>
     )
 };
