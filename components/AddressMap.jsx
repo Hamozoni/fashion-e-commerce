199fad2cb04ef7c9addd952d44787451d96c9646
Mohamed Yahia <m.yahya199290@gmail.com>
2024-06-08 23:15:31 +0300

    modified:   components/AddressMap.jsx

--- components/AddressMap.jsx
+++ components/AddressMap.jsx
@@ -3,45 +3,73 @@
 import {
     APIProvider,
     Map,
-    AdvancedMarker
+    AdvancedMarker,
+    Marker
 } from "@vis.gl/react-google-maps"
-import {useMemo, useState } from "react";
+import {useRef, useState } from "react";
 
 function AddressMap() {
 
-    const [position,setPosition] = useState({lat: 26.3159003,lng: 50.2052888})
+    const [position,setPosition] = useState({lat: 26.3159003,lng: 50.2052888});
+    const [address,setAddress] = useState({});
 
+    const mapRef = useRef()
 
+    const currentPosition = (e)=> {
 
-    // const options = useMemo(()=> {
+        setPosition(e.detail.latLng)
+        console.log(e.detail.latLng)
 
-    //     return {
-    //     disableDefaultUI: false,
-    //     zoomControl: true,
-    //     scrollwheel: true,
-    //     draggable: true
-    //     }
-    // },[]);
+    };
+
+    const locateMe = ()=> {
+        if(navigator.geolocation){
+
+            navigator.geolocation.getCurrentPosition((e)=>{
+
+                setPosition({lat:e.coords.latitude,lng: e.coords.longitude});
+                
+            })
+                
+
+        }
+    }
 
   return (
     <section 
         className="fixed top-0 left-0 w-[100vw] h-[100vh] z-50 flex justify-center items-center">
         <div className="">
             <h4></h4>
-            <div className="w-[600px] h-[600px]" >
+            <div className="w-[600px] h-[600px] relative" >
                 <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY}
 
                 >
                     <Map 
-                        // center={position} 
+                        zoomControl={true}
+                        onCenterChanged={(e)=> {
+                            console.log(e)
+                        }}
                         defaultCenter={position}
-                        defaultZoom={13}
+                        defaultZoom={15}
                         gestureHandling={'greedy'}
                         disableDefaultUI={true}
+                        draggableCursor={true}
+                        onClick={currentPosition}
                         >
-                            <AdvancedMarker position={position}/>
+                                {/* <AdvancedMarker position={position} >
+                                     
+                                </AdvancedMarker> */}
+                                <AdvancedMarker position={position}>
+                                   {/* <FaMapMarkerAlt size={30} /> */}
+                                </AdvancedMarker>
+                                   <Marker position={position} />
                     </Map>
                 </APIProvider>
+                <button 
+                    onClick={locateMe}
+                    className="absolute bottom-7 right-7 py-2 px-4 rounded-md captialize bg-green-400 text-green-950"
+                   >locate me
+                </button>
             </div>
 
         </div>
