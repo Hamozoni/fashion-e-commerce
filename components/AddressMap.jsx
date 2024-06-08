aca7b530d93060bb7b844cfaad2743be1f216c59
Mohamed Yahia <m.yahya199290@gmail.com>
2024-06-08 21:29:47 +0300

    modified:   components/AddressMap.jsx

--- components/AddressMap.jsx
+++ components/AddressMap.jsx
@@ -9,26 +9,26 @@ import {useMemo, useState } from "react";
 
 function AddressMap({onClick}) {
 
-    // const [position,setPosition] = useState({lat: 26.3159003,lng: 50.2052888})
+    const [position,setPosition] = useState({lat: 26.3159003,lng: 50.2052888})
 
 
 
-    // const options = useMemo(()=> {
+    const options = useMemo(()=> {
 
-    //     return {
-    //     disableDefaultUI: false,
-    //     zoomControl: true,
-    //     scrollwheel: true,
-    //     draggable: true
-    //     }
-    // },[]);
+        return {
+        disableDefaultUI: false,
+        zoomControl: true,
+        scrollwheel: true,
+        draggable: true
+        }
+    },[]);
 
   return (
     <section onClick={onClick} 
         className="fixed top-0 left-0 w-[100vw] h-[100vh] z-50 flex justify-center items-center">
         <div className="">
             <h4></h4>
-            {/* <div className="w-[600px] h-[600px]" >
+            <div className="w-[600px] h-[600px]" >
                 <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY}>
                     <Map 
                         center={position} 
@@ -38,7 +38,7 @@ function AddressMap({onClick}) {
                             <AdvancedMarker position={position}/>
                     </Map>
                 </APIProvider>
-            </div> */}
+            </div>
 
         </div>
     </section>
