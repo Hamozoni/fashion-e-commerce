e18bfdb6f5bdeeddd221cb7cd5723c0220f42697
Mohamed Yahia <m.yahya199290@gmail.com>
2024-06-15 02:16:23 +0300

    modified:   app/(protected)/checkout/_components/payment.jsx

--- app/(protected)/checkout/_components/payment.jsx
+++ app/(protected)/checkout/_components/payment.jsx
@@ -15,6 +15,28 @@ function Payment() {
                 <UserField name='email'/>
                 <UserField name='address'/>
             </div>
+            <div className="">
+                <label htmlFor="">name on card</label>
+                <input type="text" placeholder="cardhoder name" />
+            </div>
+            <div className="">
+                <label htmlFor="">card number</label>
+                <input type="number" placeholder="card number" />
+            </div>
+            <div className="">
+                <div className="">
+                    <label htmlFor="">month</label>
+                    <input type='number' placeholder="05"  />
+                </div>
+                <div className="">
+                    <label htmlFor="">year</label>
+                    <input type='number' placeholder="28"/>
+                </div>
+                <div className="">
+                    <label htmlFor="">cvv</label>
+                    <input type='number' placeholder="cvv"/>
+                </div>
+            </div>
         </form>
     </div>
   )
