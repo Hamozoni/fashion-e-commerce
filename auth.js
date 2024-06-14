2104178cfbdef7bb43d1219a28ec58c46197683b
Mohamed Yahia <m.yahya199290@gmail.com>
2024-06-14 12:55:01 +0300

    modified:   auth.js

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import authConfig from "./auth.config"
 import {db} from "./lip/db";
-import { findUserById } from "./lip/user";
+import { findUserAddressByEmail, findUserById } from "./lip/user";
 
  
 export const { handlers, auth,signIn,signOut } = NextAuth({
@@ -49,11 +49,14 @@ export const { handlers, auth,signIn,signOut } = NextAuth({
       if(session.user && token.role){
         session.user.role = token.role
       }
-      // if(session.user) {
-      //   session.user.email = token.email;
-      //   session.user.name,e = token.name;
-      //   session.user.image = token.image;
-      // }
+      if(session.user) {
+        // session.user.email = token.email;
+        // session.user.name,e = token.name;
+        // session.user.image = token.image;
+        if(token.address){
+          session.user.address = token.address;
+        }
+      }
 
       return session;
     },
@@ -62,10 +65,15 @@ export const { handlers, auth,signIn,signOut } = NextAuth({
       if(!token.sub) return token;
 
       const existingUser = await findUserById(token.sub);
-
+      
       if(!existingUser) return token;
+      const existingAddress = await findUserAddressByEmail(existingUser.email);
 
       token.role = existingUser.role;
+
+      if(existingAddress) {
+        token.address = existingAddress
+      }
       // token.image = existingUser.image;
       // token.name = existingUser.name;
       // token.email = existingUser.email;
