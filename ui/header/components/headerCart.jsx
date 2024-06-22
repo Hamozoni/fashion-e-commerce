e6fae5b781c3e6865fd191b1dcec368f4a3071ae
Mohamed Yahia <m.yahya199290@gmail.com>
2024-06-22 13:45:58 +0300

    modified:   app/globals.css         modified:   ui/header/components/headerCart.jsx

--- ui/header/components/headerCart.jsx
+++ ui/header/components/headerCart.jsx
@@ -8,7 +8,7 @@ function headerCart() {
     const totalItemsOnCart = useAppSelector((state)=> state.cart.totalQuantity);
 
     const className = {
-        cartNumb: "absolute left-1/3 bottom-3 animate-pingOnce bg-green-800 text-slate-100 rounded-full w-5 h-5 flex items-center justify-center p-1  "
+        cartNumb: "absolute left-0 top-0 w-[20px] h-[20px] rounded-full bg-green-900 text-green-50 flex justify-center items-center text-sm"
     };
 
   return (
@@ -17,11 +17,11 @@ function headerCart() {
               className="relative cursor-pointer">
             {
                 totalItemsOnCart ?
-                <span
+                <div
                     className={className.cartNumb}
                     >
                         {totalItemsOnCart > 9 ? "9+" : totalItemsOnCart}
-                </span>
+                </div>
                 :''
             }
             <PiShoppingCart size={28}/>
