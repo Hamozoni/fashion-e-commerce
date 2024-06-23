0ad3d1b077d7345681a9c1a34f41d75164045370
Mohamed Yahia <m.yahya199290@gmail.com>
2024-06-23 23:05:29 +0300

    modified:   ui/header/components/mobileMenu.jsx

--- ui/header/components/mobileMenu.jsx
+++ ui/header/components/mobileMenu.jsx
@@ -1,17 +1,31 @@
+"use client";
 import { TiThMenuOutline } from "react-icons/ti";
+import Categories from "../../../components/Categories";
+import { useState } from "react";
+import Overlay from "../../../components/Overlay";
 const MobileMenu = () => {
 
+  const [isCategory,setIsCategory] = useState(false)
+
     const className = {
-        menuContainer: '',
+        menuContainer: 'fixed left-1 top-1 z-50 w-full',
     }
   return (
     <section>
-        <h3>
+        <h3 onClick={()=> setIsCategory(true)} className="cursor-pointer">
             <TiThMenuOutline size={22} />
         </h3>
-        <div className={className.menuContainer}>
-            
-        </div>
+        {
+          isCategory && (
+            <>
+              <Overlay onClick={()=> setIsCategory(false)}/>
+              <div className={className.menuContainer}>
+                  <Categories/>
+              </div>
+            </>
+
+          )
+        }
     </section>
   )
 }
