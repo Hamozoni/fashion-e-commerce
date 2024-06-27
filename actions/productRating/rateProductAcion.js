e66e5a8592b66c10527826340495864bb028d36c
Mohamed Yahia <m.yahya199290@gmail.com>
2024-06-27 18:42:51 +0300

    modified:   actions/productRating/rateProductAcion.js

--- actions/productRating/rateProductAcion.js
+++ actions/productRating/rateProductAcion.js
@@ -1,4 +1,6 @@
 "use server";
+import { fs } from "fs/promises";
+import { db } from "../../lip/db";
 import { ratingSchema } from "../../validationSchemas/ratingSchema"
 
 export const rateProduct = async(formData)=> {
@@ -19,8 +21,35 @@ export const rateProduct = async(formData)=> {
     };
     
     if(validateForm.success) {
-        console.log(data);
-        return {success: "review has been committed successfuly"}
+
+        if(data.reviewImage){
+            try{
+                await fs.mkdir('public/reviewsImages',{recursive: true});
+                const imagepath = `public/reviewsImages/${crypto.randomUUID()}_${data?.reviewImage?.name}`;
+
+                await fs.writeFile(imagepath,Buffer.from(await data?.reviewImage?.arrayBuffer()))
+
+                data.reviewImage = imagepath;
+
+            }
+            catch {
+                return {error: "images something went wrong"}
+            }
+        }
+
+        try {
+
+            await db.reviews.create({data:{
+                ...data
+            }})
+
+            console.log(data)
+
+            return {success: "review has been committed successfuly"}
+        }
+        catch {
+            return {error: "something went wrong"}
+        }
     }
 
 
