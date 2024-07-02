fb477ac42c478ba4c0bb5cc6a4f7921aa59fc537
Mohamed Yahia <m.yahya199290@gmail.com>
2024-07-02 06:48:29 +0300

    modified:   app/product/_components/ImagesGalary.jsx

--- app/product/_components/ImagesGalary.jsx
+++ app/product/_components/ImagesGalary.jsx
@@ -4,14 +4,14 @@ import { useContext, useEffect, useState } from "react";
 import { ProductDetailsContext } from "./ProductDetails";
 
 
-function ImagesGalary() {
+function ImagesGalary({productImages}) {
 
-    const {product,selectedColor} = useContext(ProductDetailsContext)
-    const [selectedImage,setSelectedImage] = useState(product.images[selectedColor][0]?.imagePath);
+    const {selectedColor} = useContext(ProductDetailsContext)
+    const [selectedImage,setSelectedImage] = useState(productImages[selectedColor][0]?.imagePath);
 
     useEffect(()=> {
 
-        setSelectedImage(product.images[selectedColor][0]?.imagePath);
+        setSelectedImage(productImages[selectedColor][0]?.imagePath);
 
     },[selectedColor])
 
@@ -21,7 +21,7 @@ function ImagesGalary() {
         <div className="flex items-center gap-3 sticky top-[80px] left-0">
             <div className="flex items-center justify-center gap-2 flex-col ">
                 {
-                    product.images[selectedColor]?.map((img)=> (
+                    productImages[selectedColor]?.map((img)=> (
                         img.color === selectedColor ? 
                         <Image 
                             className={`rounded-md border border-gray-100 shadow-md mb-2 min-w-[60px] max-w-[60px]  min-h-[60px] max-h-[60px] ${selectedImage === img?.imagePath && 'border border-green-900'} cursor-pointer`}
