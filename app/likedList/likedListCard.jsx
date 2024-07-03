42778ed506c1034aca77e88fb08bf5f2b7a2074a
Mohamed Yahia <m.yahya199290@gmail.com>
2024-07-03 06:55:21 +0300

    modified:   app/likedList/likedListCard.jsx

--- app/likedList/likedListCard.jsx
+++ app/likedList/likedListCard.jsx
@@ -1,15 +1,18 @@
 "use client";
 
 import ProductCard from "../../ui/productCard/ProductCard"
-import {useAppSelector} from "../../store/store";
-
+import {useAppDispatch, useAppSelector} from "../../store/store";
+import { ButtonWithIcon } from "../../components/buttons";
+import { removeAllFromLikedList } from "../../store/features/likedListSlice";
+import { MdOutlineDeleteOutline } from "react-icons/md";
 const LikedListCard = () => {
 
-    const likedList = useAppSelector(state=> state.likedList)
+    const likedList = useAppSelector(state=> state.likedList);
+    const dispatch = useAppDispatch
   return (
     <div>
         {
-            likedList ? (
+            !!likedList ? (
                 likedList?.map(product=> (
                     <ProductCard key={product?.id} product={product} />
                 ))
@@ -19,6 +22,12 @@ const LikedListCard = () => {
                 </div>
             )
         }
+        <ButtonWithIcon
+           text='delete all'
+           type='delete'
+           Icon={MdOutlineDeleteOutline}
+           onClick={()=>dispatch(removeAllFromLikedList())}
+         />
         
     </div>
   )
