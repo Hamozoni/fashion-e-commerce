c9b43fc0d7a97290dacf05b1e1b078ffb1dfb2aa
Mohamed Yahia <m.yahya199290@gmail.com>
2024-07-04 07:17:37 +0300

    modified:   ui/home/slider.jsx

--- ui/home/slider.jsx
+++ ui/home/slider.jsx
@@ -19,21 +19,28 @@ const categories = [
 function Slider() {
   return (
     <div className="-translate-y-16 max-h-lvh relative">
-        <div className="flex items-center min-w-fit h-lvh  max-h-lvh ">
+        <div className="flex items-center min-w-fit h-lvh  max-h-lvh opacity-55 ">
             <Image 
-                className="w-full object-cover h-lvh max-h-lvh  min-w-full " 
+                className="w-full object-cover h-lvh max-h-lvh  min-w-full" 
                 src={slide9} 
                 alt="slide image"
                 />
         </div>
         <div className=" absolute left-0 top-0 w-full h-full flex items-center justify-center">
             <div className="p-3 lg:px-8 max-w-full overflow-hidden">
-                <div className="min-w-fit">
+                <div className="min-w-fit flex">
                     {
                         categories?.map(({name,desc})=> (
-                            <section className="mi-w-full">
-                                <h2>{name}</h2>
-                                <span>{desc}</span>
+                            <section className="min-w-full p-4 font-extrabold lg:p-8 capitalize text-center text-green-950 ">
+                                <h2 
+                                    className="text-5xl"
+                                    >{name}
+                                </h2>
+                                <p className="text-2xl my-4">{desc}</p>
+                                <button
+                                    className="text-2xl uppercase"
+                                    >start shopping
+                                </button>
                             </section>
                         ))
                     }
