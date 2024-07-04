dfb08872871ad7957f318d02217d2784beec0115
Mohamed Yahia <m.yahya199290@gmail.com>
2024-07-04 07:59:41 +0300

    modified:   app/globals.css         modified:   tailwind.config.js         modified:   ui/home/slider.jsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,8 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-to-transparent":
+        "linear-gradient(to right,rgba(250 250 250 / 80%) ,rgba(250 250 250 / 50%) ,rgba(250 250 250 / 30%))"
       },
       animation: {
         pingOnce: "pingOnce 0.3s cubic-bezier(0, 0, 0.2, 1)"
--- ui/home/slider.jsx
+++ ui/home/slider.jsx
@@ -19,12 +19,13 @@ const categories = [
 function Slider() {
   return (
     <div className="-translate-y-16 max-h-lvh relative">
-        <div className="flex items-center min-w-fit h-lvh  max-h-lvh opacity-55 ">
+        <div className="flex items-center min-w-fit h-lvh  max-h-lvh ">
             <Image 
                 className="w-full object-cover h-lvh max-h-lvh  min-w-full" 
                 src={slide9} 
                 alt="slide image"
                 />
+            <div className="absolute left-0 top-0 w-full h-full bg-gradient-transparent" ></div>
         </div>
         <div className=" absolute left-0 top-0 w-full h-full flex items-center justify-center">
             <div className="p-3 lg:px-8 max-w-full overflow-hidden">
@@ -36,9 +37,9 @@ function Slider() {
                                     className="text-5xl"
                                     >{name}
                                 </h2>
-                                <p className="text-2xl my-4">{desc}</p>
+                                <p className="text-2xl my-4 max-w-580px">{desc}</p>
                                 <button
-                                    className="text-2xl uppercase"
+                                    className="text-2xl uppercase p-3 rounded-md border border-green-900 hover:scale-105"
                                     >start shopping
                                 </button>
                             </section>
