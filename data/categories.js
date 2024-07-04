fa80b42a60481bd7d34b10491c2dabc102d7fb20
Mohamed Yahia <m.yahya199290@gmail.com>
2024-07-04 22:42:49 +0300

    data/         public/categories/subCategory/kids/jeans.png

---
+++ data/categories.js
@@ -0,0 +1,31 @@
+// categories images
+import men from "../public/categories/category/men.png";
+import women from "../public/categories/category/womem.png";
+import kids from "../public/categories/category/kids.png";
+
+// sub categories images
+
+// men images
+import menJacket from "../public/categories/subCategory/men/jacket.png";
+import menJeans from "../public/categories/subCategory/men/jeans.png";
+import menNightWear from "../public/categories/subCategory/men/shirt.png";
+import menShirt from "../public/categories/subCategory/men/shirt.png";
+import menTShirt from "../public/categories/subCategory/men/t-shirt.png";
+// women images
+import womenDress from "../public/categories/subCategory/women/dress.png";
+import womenJacket from "../public/categories/subCategory/women/jacket.png";
+import womenJeans from "../public/categories/subCategory/women/jeans.png";
+import womenNightWear from "../public/categories/subCategory/women/nightWear.png";
+import womenShoes from "../public/categories/subCategory/women/shoes.png";
+import womenSkirt from "../public/categories/subCategory/women/skirt.png";
+// kids
+import kidsDress from "../public/categories/subCategory/kids/dress.png";
+import kidsJacket from "../public/categories/subCategory/kids/jacket.png";
+import kidsJeans from "../public/categories/subCategory/kids/jeans.png";
+import kidsShirt from "../public/categories/subCategory/kids/shirt.png";
+import kidsShoes from "../public/categories/subCategory/kids/shoes.png";
+import kidsTSkirt from "../public/categories/subCategory/kids/t-shirt.png";
+
+
+// export const 
+
