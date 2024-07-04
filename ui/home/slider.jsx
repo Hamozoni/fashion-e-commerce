f682cf002a3d2e97a1b5279cda8355330773d7c2
Mohamed Yahia <m.yahya199290@gmail.com>
2024-07-04 11:29:25 +0300

    modified:   ui/home/slider.jsx

--- ui/home/slider.jsx
+++ ui/home/slider.jsx
@@ -1,6 +1,8 @@
+"use client";
 import Image from "next/image";
 import slide9 from "../../public/slide/slide9.jpg";
 import { IoIosArrowForward ,IoIosArrowBack} from "react-icons/io";
+import { useState } from "react";
 
 const categories = [
     {
@@ -23,6 +25,20 @@ const className = {
 
 function Slider() {
 
+    const [sliderIndex,setSliderIndex] = useState(0);
+
+    const handleNext = ()=> {
+        if(sliderIndex < categories?.length - 1) {
+            setSliderIndex(prev=> prev + 1)
+        }
+    }
+
+    const handlePrev = ()=> {
+        if(sliderIndex > 0) {
+            setSliderIndex(prev=> prev - 1)
+        }
+    }
+
   return (
     <div className="-translate-y-16 max-h-lvh relative">
         <div className="flex items-center min-w-fit h-lvh  max-h-lvh ">
@@ -34,30 +50,43 @@ function Slider() {
             <div className="absolute left-0 top-0 w-full h-full bg-gradient-transparent" ></div>
         </div>
         <div className=" absolute left-0 top-0 w-full h-full flex items-center justify-center">
-            <div className="p-3 lg:px-8 max-w-full overflow-hidden">
-                <div className="min-w-fit flex">
+            <div className="">
+                <div className="p-3 lg:px-8 max-w-full overflow-hidden" style={{transform: `translateX(${(sliderIndex * categories?.length) / 100}%)`}}>
+                    <div className="min-w-fit flex">
+                        {
+                            categories?.map(({name,desc})=> (
+                                <section key={name} className="min-w-full p-4 font-extrabold lg:p-8 capitalize text-center text-green-950 ">
+                                    <h2 
+                                        className="text-5xl"
+                                        >{name}
+                                    </h2>
+                                    <p className="text-2xl my-4 max-w-[580px] mx-auto">{desc}</p>
+                                    <button
+                                        className="text-xl  p-2 px-5 capitalize rounded-md border border-green-900 hover:scale-105"
+                                        >start shop
+                                    </button>
+                                </section>
+                            ))
+                        }
+                    </div>
+                </div>
+                <ul className="flex items-center justify-center gap-2 mb-5">
                     {
-                        categories?.map(({name,desc})=> (
-                            <section className="min-w-full p-4 font-extrabold lg:p-8 capitalize text-center text-green-950 ">
-                                <h2 
-                                    className="text-5xl"
-                                    >{name}
-                                </h2>
-                                <p className="text-2xl my-4 max-w-[580px] mx-auto">{desc}</p>
-                                <button
-                                    className="text-xl  p-2 px-5 capitalize rounded-md border border-green-900 hover:scale-105"
-                                    >start shop
-                                </button>
-                            </section>
+                        categories?.map(({name},index)=> (
+                            <li 
+                                onClick={()=> setSliderIndex(index)}
+                                className={`w-6 h-3 border-2 border-green-300 rounded-md cursor-pointer  ${index === sliderIndex ? 'bg-green-950 w-8 h-4' :''}`}
+                                key={name}></li>
                         ))
                     }
-                </div>
+                </ul>
+
             </div>
         </div>
-        <button className={`${className.prevNextBtn} right-3 lg:right-8`}>
+        <button onClick={handleNext} className={`${className.prevNextBtn} right-3 lg:right-8`}>
             <IoIosArrowForward size={20}/>
         </button>
-        <button className={`${className.prevNextBtn} left-3 lg:left-8`}>
+        <button onClick={handlePrev} className={`${className.prevNextBtn} left-3 lg:left-8`}>
             <IoIosArrowBack size={20} />
         </button>
     </div>
