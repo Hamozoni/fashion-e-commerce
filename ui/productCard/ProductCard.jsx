cee35e51d3221a3addf1a0861077427fcc454d90
Mohamed Yahia <m.yahya199290@gmail.com>
2024-07-05 20:52:15 +0300

    modified:   ui/productCard/ProductCard.jsx

--- ui/productCard/ProductCard.jsx
+++ ui/productCard/ProductCard.jsx
@@ -12,7 +12,8 @@ import getCurrency from "../../lip/getCurrency";
 import { arrayGroupBykey } from "../../lip/arrayGroupBykey";
 // icons
 import { IoIosArrowForward,IoIosArrowBack} from "react-icons/io";
-import { useRouter } from "next/navigation";
+import { FaEye } from "react-icons/fa";
+import { TbEyeClosed } from "react-icons/tb";
 
 
 function ProductCard({product}) {
@@ -25,12 +26,12 @@ function ProductCard({product}) {
     const [selectedColor,setSelectedColor] = useState(images[0]?.color);
     const [selectedSize,setSelectedSize] = useState(sizes[0]?.name);
     const [imagesIndex,setImagesIndex] = useState(0)
-    const router = useRouter()
+    const [isLikHaverd,setIsLikHaverd] = useState(false)
 
     const className = {
             card: 'w-[344px] rounded-lg border border-teal-100 cursor-pointer hover:border-teal-200 relative p-3 bg-teal-50',
             image: 'w-fit flex-1 min-w-[320px] max-h-[370px] max-w-[320px]',
-            heart: 'absolute top-4 right-4',
+            heart: 'absolute top-2 left-0 w-full flex items-center justify-between p-4',
             iamgebaginationBtn: 'bg-green-50 text-green-900 absolute top-1/2 hover:scale-125 -translate-y-1/2 w-8 h-10 flex items-center justify-center'
     }
 
@@ -56,7 +57,6 @@ function ProductCard({product}) {
     <div className={className.card}>
         <div  className=" w-[320px] overflow-hidden relative group">
             <div
-                onClick={()=> router.push(`/product/${id}`)}
                 className="flex items-center" 
                 style={{transform: `translateX(-${imagesIndex * 100}%)`}}
                 >
@@ -78,11 +78,12 @@ function ProductCard({product}) {
                     
                 }
             </div>
+            <div className=" absolute left-0 top-0 w-full h-0  group-hover:h-full bg-gradient-transparent "></div>
             {
                 (imagesIndex === productImages[selectedColor]?.length - 1) ? null :
                 <button
                     onClick={handleNext} 
-                    className={`${className.iamgebaginationBtn} right-0 translate-x-full group-hover:translate-x-0`}>
+                    className={`${className.iamgebaginationBtn} right-0 translate-x-full group-hover:translate-x-0 rounded-tl-full rounded-bl-full`}>
                 <IoIosArrowForward sizes={20} />
                 </button>
             }
@@ -90,27 +91,41 @@ function ProductCard({product}) {
                 imagesIndex === 0 ? null :
                 <button
                     onClick={handlePrev} 
-                    className={`${className.iamgebaginationBtn} left-0 -translate-x-full group-hover:translate-x-0`}>
+                    className={`${className.iamgebaginationBtn} left-0 -translate-x-full group-hover:translate-x-0 rounded-tr-full rounded-br-full`}>
                 <IoIosArrowBack sizes={20}/>
                 </button>
             }
-                    <ul className=" absolute left-0 bottom-0 w-full translate-y-full group-hover:translate-y-0 flex items-center justify-center gap-2 py-3">
-                {
-                    productImages[selectedColor]?.map(({id,color},index)=> (
-                        <li 
-                            onClick={()=> setImagesIndex(index)}
-                            key={id} 
-                            className=" w-4 h-4 rounded-full border border-teal-500 hover:scale-125 shadow-md" 
-                            style={ index === imagesIndex ? { backgroundColor: color,width: '24px',height: '24px'}: {}}
-                            >
-                        </li>
+            <div className=" absolute left-0 bottom-0 w-full translate-y-full group-hover:translate-y-0">
+                <ul className="flex items-center justify-center gap-2 py-3">
+                    {
+                        productImages[selectedColor]?.map(({id,color},index)=> (
+                            <li 
+                                onClick={()=> setImagesIndex(index)}
+                                key={id} 
+                                className=" w-4 h-4 rounded-full border border-teal-500 hover:scale-125 shadow-md" 
+                                style={ index === imagesIndex ? { backgroundColor: color,width: '24px',height: '24px'}: {}}
+                                >
+                            </li>
 
-                    ))
-                }
-        </ul>
+                        ))
+                    }
+               </ul>
+                <AddToCart 
+                    product={product} 
+                    selectedColor={selectedColor}
+                    selectedSize={selectedSize} 
+                    />
+            </div>
         </div>
         <div className={className.heart}>
             <AddToListBtn product={product} />
+            <Link 
+                onMouseEnter={()=> setIsLikHaverd(true)}
+                onMouseLeave={()=> setIsLikHaverd(false)}
+                href={`/product/${id}`} 
+                className="flex justify-center items-center p-2 text-teal-600 bg-teal-50 rounded-full border border-teal-100">
+               {isLikHaverd ? <TbEyeClosed size={22}/> : <FaEye size={22}/> }
+            </Link>
         </div>
         <div className="p-3">
             <div className="pb-2">
@@ -135,11 +150,6 @@ function ProductCard({product}) {
                 setSelectedSize={setSelectedSize}
                  />
             </div>
-            <AddToCart 
-                product={product} 
-                selectedColor={selectedColor}
-                selectedSize={selectedSize} 
-                />
         </div>
     </div>
   )
