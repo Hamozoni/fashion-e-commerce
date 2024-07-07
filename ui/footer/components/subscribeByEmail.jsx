1fe471ea789fd6371cd49e071016d5b6a0207ab9
Mohamed Yahia <m.yahya199290@gmail.com>
2024-07-07 18:12:40 +0300

    modified:   ui/footer/components/subscribeByEmail.jsx         modified:   ui/footer/footer.jsx

--- ui/footer/components/subscribeByEmail.jsx
+++ ui/footer/components/subscribeByEmail.jsx
@@ -1,11 +1,14 @@
 
 export const  SubscribeByEmail = ()=> {
     return (
-        <div className="">
-            <h4>Get our latest offers and news straight in your inbox.</h4>
-            <form action="">
-                <input type="text" placeholder="place enter an email address" />
-                <button>subscibe</button>
+        <div className="capitalize my-3">
+            <h4 className="text-teal-50 text-lg mb-3">Get our latest offers and news straight in your inbox.</h4>
+            <form className="bg-teal-950 rounded-full border border-teal-600 flex gap-1 p-1" action="">
+                <input 
+                    className="rounded-full text-teal-50 w-full border-2 border-teal-900 px-4 bg-transparent focus:border-teal-600" 
+                    type="text" placeholder="place enter an email address"
+                     />
+                <button className="rounded-full p-2 font-bold text-lg text-teal-900 max-h-full px-4 bg-white capitalize">subscibe</button>
             </form>
         </div>
     )
--- ui/footer/footer.jsx
+++ ui/footer/footer.jsx
@@ -2,20 +2,28 @@ import Link from "next/link"
 // components
 import {FooterCategories} from "./components/footerCatgories";
 import {FooterAboutWebate} from "./components/footerAboutWesite";
-import {DownloadApps} from "./components/downloadApps"
+import {DownloadApps} from "./components/downloadApps";
+import {SubscribeByEmail} from "./components/subscribeByEmail"
+import { CartFooter } from "../../app/cart/_components/CartSummary";
 
 export const Footer = ()=> {
     return (
         <footer className="bg-teal-950">
             <div className="p-3 lg:px-8">
-               <div className="flex gap-10">
+               <div className="flex gap-10 border-b border-teal-700 pb-5 mb-5">
+                    <div className="">
+                        <DownloadApps />
+                        <SubscribeByEmail />
+                    </div>
                     <FooterCategories />
                     <FooterAboutWebate />
-                    <DownloadApps />
                </div>
+               <div className="flex gap-5">
                 <Link className=" text-teal-50 capitalize text-lg lg:text-3xl font-extrabold" href='/'
-                  >system
-               </Link> 
+                    >system
+                </Link> 
+                <CartFooter />
+               </div>
             </div>
         </footer>
     )
