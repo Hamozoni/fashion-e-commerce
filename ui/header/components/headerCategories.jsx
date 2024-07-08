eb9afccfa0bc54d1bfa4f2958ffe3c7d7752e04b
Mohamed Yahia <m.yahya199290@gmail.com>
2024-07-08 14:32:18 +0300

    modified:   ui/header/components/headerCategories.jsx         modified:   ui/header/components/mobileMenu.jsx

--- ui/header/components/headerCategories.jsx
+++ ui/header/components/headerCategories.jsx
@@ -1,12 +1,17 @@
 "use client";
-
-import { useState } from "react";
-import { categoriesData } from "../../../data/categoriesData";
-import Overlay from "../../../components/Overlay";
 import Image from "next/image";
 import Link from "next/link";
-
+import { useState } from "react";
+// categories data
+import { categoriesData } from "../../../data/categoriesData";
+// components
+import Overlay from "../../../components/Overlay";
+// icons
 import { GiTireIronCross } from "react-icons/gi";
+// styles
+const className ={ 
+    cateLi : ' capitalize font-bold text-teal-800 text-xl cursor-pointer'
+};
 
 export const SubCatories = ({subCategories,setIsSubCategories})=> {
     return (
@@ -16,7 +21,7 @@ export const SubCatories = ({subCategories,setIsSubCategories})=> {
                 <li 
                     onClick={()=> setIsSubCategories(false)}
                     key={name} 
-                    className="min-w-[300px] border-2 border-gray-200 rounded-full mb-3 hover:bg-teal-100 hover:border-teal-400">
+                    className="w-full border-2 border-gray-200 rounded-full mb-3 hover:bg-teal-100 hover:border-teal-400">
                     <Link 
                         className="flex items-center gap-3 p-1"
                         href={linkPath}>
@@ -38,43 +43,42 @@ export const SubCatories = ({subCategories,setIsSubCategories})=> {
 }
 
 
-export const CategoriesModel = ({subCategories,categoryName,setIsSubCategories})=> {
-    return (
-        <>
-        <Overlay onClick={()=> setIsSubCategories(false)} />
-        <div className="fixed top-[70px] left-3 lg:left-8 border border-teal-300  rounded-md bg-teal-50 z-50">
-           <nav className="">
-               <div 
-                   className="px-5 py-3 cursor-pointer"
-                   onClick={()=> setIsSubCategories(false)}
-                   ><GiTireIronCross />
-               </div>
-               <div className="relative p-3 gap-3">
-                   <div className="absolute top-0 left-0 w-full h-full bg-gradient-transparent"></div>
-                   <h3 
-                       className=" text-center text-teal-950 text-xl font-bold"
-                       >{categoryName} fashion
-                   </h3>
-               </div>
-               <SubCatories 
-                    subCategories={subCategories}
-                    setIsSubCategories={setIsSubCategories}
-                   />
-           </nav>
-        </div>
-       </>
-    )
-}
 
 export const HeaderCategories = ()=> {
-
+    
     const [isSubCategories,setIsSubCategories] = useState(false);
     const [subCategories,setSubCategories] = useState(null);
     const [categoryName,setCategoryName] = useState('');
+    
 
-    const className ={ 
-        cateLi : ' capitalize font-bold text-teal-800 text-xl cursor-pointer'
-    }
+    const CategoriesModel = ()=> {
+        return (
+            <>
+            <Overlay onClick={()=> setIsSubCategories(false)} />
+            <div className="fixed top-[70px] left-3 lg:left-8 border min-w-[300px] border-teal-100 shadow-md  rounded-md bg-gray-50 z-[70]">
+               <nav className="">
+                   <div 
+                       className="px-5 py-3 cursor-pointer"
+                       onClick={()=> setIsSubCategories(false)}
+                       ><GiTireIronCross />
+                   </div>
+                   <div className="relative p-3 gap-3">
+                       <div className="absolute top-0 left-0 w-full h-full bg-gradient-transparent"></div>
+                       <h3 
+                           className=" text-center text-teal-950 text-xl font-bold"
+                           >{categoryName} fashion
+                       </h3>
+                   </div>
+                   <SubCatories 
+                        subCategories={subCategories}
+                        setIsSubCategories={setIsSubCategories}
+                       />
+               </nav>
+            </div>
+           </>
+        )
+    };
+
     return (
         <div className=" capitalize">
             <ul className="flex items-center gap-3">
@@ -94,11 +98,7 @@ export const HeaderCategories = ()=> {
             </ul>
             {
                 isSubCategories ?
-                <CategoriesModel
-                    subCategories={subCategories}
-                    categoryName={categoryName}
-                    setIsSubCategories={setIsSubCategories}
-                 />
+                <CategoriesModel />
                 : null
             }
         </div>
--- ui/header/components/mobileMenu.jsx
+++ ui/header/components/mobileMenu.jsx
@@ -1,40 +1,59 @@
 "use client";
 import { useState } from "react";
 // components
+import { SubCatories } from "./headerCategories";
 import Overlay from "../../../components/Overlay";
 // icons
 import { TiThMenuOutline } from "react-icons/ti";
 import { GiTireIronCross } from "react-icons/gi";
+import { IoIosArrowForward } from "react-icons/io";
 // categories data
 import { categoriesData } from "../../../data/categoriesData";
+import Image from "next/image";
 
-export const MobileCategories = ()=> {
-  return (
-    <>
-       <Overlay />
-      <div className=" fixed left-3 lg:left-8 top-[70px] w-[300px] bg-teal-50 rounded-md border border-teal-200">
-          <header>
-              <h4>categories</h4>
-              <button><GiTireIronCross/></button>
-          </header>
-          <ul className="">
-               {
-                  categoriesData?.map(({name,imagePath})=>(
-                    <li key={name} className="">
-
-                    </li>
-                  ))
-               }
-          </ul>
-      </div>
-    </>
-  )
-}
 
 const MobileMenu = () => {
-
-    const [isCategory,setIsCategory] = useState(false)
-
+  
+  const [isCategory,setIsCategory] = useState(false);
+  const [openedSubCategoyNmae,setOpenedSubCategoyNmae] = useState(null)
+  
+  const MobileCategories = ()=> {
+    return (
+      <>
+         <Overlay onClick={()=> setIsCategory(false)} />
+        <div className="capitalize fixed min-w-[300px] left-3 z-[70] lg:left-8 top-[70px] w-[300px] bg-gray-50 rounded-md border border-teal-100 shadow-md">
+            <header className="flex items-center justify-between p-3">
+                <h4 className="text-teal-950 text-xl font-bold">categories</h4>
+                <button><GiTireIronCross/></button>
+            </header>
+            <div className="h-[500px] max-h-[500px] overflow-y-auto">
+              <ul className="py-3 h-fit min-h-fit">
+                  {
+                      categoriesData?.map(({name,imagePath,sub},index)=>(
+                        <li key={name} className="px-3" onClick={()=> setOpenedSubCategoyNmae(index)}>
+                          <div className="flex items-center justify-between cursor-pointer rounded-md p-3 border-2 border-gray-200 hover:bg-gray-100">
+                            <div className="flex items-center gap-2">
+                              <Image className='rounded-md' src={imagePath} width={50} alt={name}/>
+                              <h5 className="text-teal-950 font-bold text-xl">{name} fashion</h5>
+                            </div>
+                            <IoIosArrowForward />
+                          </div>
+                          {
+                            openedSubCategoyNmae === index &&
+                            <SubCatories 
+                              subCategories={sub}
+                              setIsSubCategories={setIsCategory}
+                              />
+                          }
+                        </li>
+                      ))
+                  }
+              </ul>
+            </div>
+        </div>
+      </>
+    )
+  }
     const className = {
         menuContainer: 'flex items-center justify-center fixed left-0 z-50 w-full bg-green-100',
     };
@@ -44,6 +63,10 @@ const MobileMenu = () => {
         <h3 onClick={()=> setIsCategory(true)} className="cursor-pointer">
             <TiThMenuOutline size={26} />
         </h3>
+        {
+           isCategory &&
+          <MobileCategories />
+        }
     </section>
   )
 }
