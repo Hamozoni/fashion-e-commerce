4cb45011d318e477b8b091c5a17c94a08b06cc1a
Mohamed Yahia <m.yahya199290@gmail.com>
2024-07-08 20:44:20 +0300

    modified:   app/cart/_components/CartItemsCard.jsx

--- app/cart/_components/CartItemsCard.jsx
+++ app/cart/_components/CartItemsCard.jsx
@@ -6,9 +6,14 @@ import { useAppDispatch } from "../../../store/store";
 import { MdOutlineDeleteOutline } from "react-icons/md";
 import Link from "next/link";
 import { ButtonWithIcon } from "../../../components/buttons";
+import { AppContext } from "../../contextProvider";
+import { useContext } from "react";
 
 function CartItemsCard({product}) {
+
     const dispatch = useAppDispatch();
+
+    const {innerWidth} = useContext(AppContext)
 
     const removeItem = ()=> {
         dispatch(removeItemFromCart({
@@ -19,74 +24,77 @@ function CartItemsCard({product}) {
     }
 
     const className = {
-        tableBodyRow :' p-3 border border-gray-100'
+        tableBodyRow :'p-3 border border-gray-100'
     }
 
   return (
-    <div className=" gap-3 p-3 mb-8 border-l-2 border-l-teal-400 rounded-md shadow-md border border-gray-100 w-full">
-        <Link 
-            href={`/product/${product?.id}`}
-            className="flex items-center justify-center mb-3 sm:mb-0">
-            <Image
-                className="max-h-[150px]" 
-                src={product?.image?.replace("public","")}
-                width={150}
-                height={150}
-                alt='product image'
-                 />
-        </Link>
-        <section>
-            <Link href={`/product/${product?.id}`}>
-                <h4 
-                    className="mb-5 text-xl font-bold text-teal-950 text-center hover:text-teal-800"
-                    >{product?.name}
-                </h4>
+    <div className="p-3 mb-8 border-l-2 border-l-teal-400 rounded-md shadow-md border border-gray-100 w-full">
+         <Link href={`/product/${product?.id}`}>
+            <h4 
+                className="mb-5 text-md sm:text-xl font-bold text-teal-950 text-center hover:text-teal-800"
+                >{product?.name}
+            </h4>
+       </Link>
+        <div className="flex gap-3">
+            <Link 
+                href={`/product/${product?.id}`}
+                className="flex items-center justify-center mb-3 sm:mb-0">
+                <Image
+                    className={`${innerWidth > 550 ? 'max-h-[200]' :'max-h-[150]'}`}
+                    src={product?.image?.replace("public","")}
+                    width={innerWidth > 550 ? 150 : 100}
+                    height={innerWidth > 550 ? 200 : 150}
+                    alt='product image'
+                    />
             </Link>
-            <div className="lg:flex gap-3">
-                <table className="rounded-md overflow-hidden">
+            <section>
+                <div className="sm:flex gap-3">
+                    <table className="rounded-md overflow-hidden">
 
-                    <thead className="pb-3">
-                        <tr className="text-teal-950 text-lg font-bold">
-                            <th className={className?.tableBodyRow}>color </th>
-                            <th className={className?.tableBodyRow}>size</th>
-                            <th className={className?.tableBodyRow}>price</th>
-                            <th className={className?.tableBodyRow}>total price</th>
+                        <thead className="pb-3">
+                            <tr className="text-teal-950 text-md sm:text-lg font-bold">
+                                <th className={className?.tableBodyRow}>color </th>
+                                <th className={className?.tableBodyRow}>size</th>
+                                <th className={className?.tableBodyRow}>price</th>
+                                <th className={className?.tableBodyRow}>total price</th>
 
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="text-teal-800 text-md font-bold">
-                            <td className={className?.tableBodyRow}> 
-                                <p 
-                                    style={{backgroundColor: product?.selectedColor}}
-                                    className="w-[35px] h-[10px] rounded-full"
-                                    >
-                                </p> 
-                            </td>
-                            <td className={className?.tableBodyRow}>{product?.selectedSize}</td>
-                            <td className={className?.tableBodyRow}>{getCurrency(product?.priceInCent)}</td>
-                            <td className={className?.tableBodyRow}>{getCurrency(product?.priceInCent * product?.quantity)}</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr className="text-teal-900 text-sm sm:text-md font-bold">
+                                <td className={className?.tableBodyRow}> 
+                                    <p 
+                                        style={{backgroundColor: product?.selectedColor}}
+                                        className="w-[35px] h-[10px] rounded-full"
+                                        >
+                                    </p> 
+                                </td>
+                                <td className={className?.tableBodyRow}>{product?.selectedSize}</td>
+                                <td className={className?.tableBodyRow}>{getCurrency(product?.priceInCent)}</td>
+                                <td className={className?.tableBodyRow}>{getCurrency(product?.priceInCent * product?.quantity)}</td>
 
-                        </tr>
-                    </tbody>
-                </table>
-                <div className="flex lg:flex-col justify-between pt-3 lg-pt-0">
-                    <QuantityBtn 
-                        id={product.id} 
-                        selectedColor={product.selectedColor} 
-                        selectedSize={product.selectedSize} 
-                        />
-                    <div className="max-w-24">
-                        <ButtonWithIcon 
-                            text='reove'
-                            type='delete'
-                            Icon={MdOutlineDeleteOutline}
-                            onClick={removeItem}
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div className="flex flex-col justify-between pt-3 lg-pt-0">
+                        <QuantityBtn 
+                            id={product.id} 
+                            selectedColor={product.selectedColor} 
+                            selectedSize={product.selectedSize} 
                             />
+                        <div className="max-w-24">
+                            <ButtonWithIcon 
+                                text='reove'
+                                type='delete'
+                                Icon={MdOutlineDeleteOutline}
+                                onClick={removeItem}
+                                />
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </div>
+
     </div>
   )
 }
