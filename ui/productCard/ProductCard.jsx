5a6852bab80c1ee8fbc480b7c9d72520e417a045
Mohamed Yahia <m.yahya199290@gmail.com>
2024-07-19 08:20:52 +0300

    modified:   ui/productCard/ProductCard.jsx

--- ui/productCard/ProductCard.jsx
+++ ui/productCard/ProductCard.jsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import {useState } from "react";
 // component
 import {AddToListBtn} from "../../components/addToListBtn";
 import {AddToCart} from "../../app/product/_components/AddToCart";
@@ -12,69 +12,60 @@ import getCurrency from "../../lip/getCurrency";
 // icons
 
 
+const className = {
+    card: 'w-[344px] rounded-lg border border-gray-100 cursor-pointer hover:border-gray-200 relative p-3',
+    image: 'w-fit flex-1 min-w-[320px] max-h-[370px] max-w-[320px]',
+    heart: 'absolute top-2 left-0 w-full flex items-center justify-between p-4',
+};
+
 function ProductCard({product}) {
 
-    
-    const [productDetails,setProductDetails] = useState(product);
-    
-    const className = {
-            card: 'w-[344px] rounded-lg border border-gray-100 cursor-pointer hover:border-gray-200 relative p-3',
-            image: 'w-fit flex-1 min-w-[320px] max-h-[370px] max-w-[320px]',
-            heart: 'absolute top-2 left-0 w-full flex items-center justify-between p-4',
-    }
+   const [productDetails,setProductDetails] = useState(product);
 
+   const {id,color,colorName,size,imagePath} = productDetails;
 
+   const linkHref = `/product/${id}?color=${color}$colorName=${colorName}&size=${size}&imagePath=${imagePath}`
+    
    return (
     <div className={className.card}>
-        <div  className=" w-[320px] overflow-hidden flex  relative group">
-            {
-                productDetails?.images?.map(({imagePath,colorName})=> (
-                    colorName === productDetails?.colorName ?
-                    <div key={productDetails?.id}
-                        className={className.image}>
-                        <Image 
-                            className="rounded-md"
-                            src={imagePath}
-                            alt='product image'
-                            width={320}
-                            height={370}
-                        />
-
-                    </div> : null
-
-                ))
-            }
-
-        </div>
+        <Link href={linkHref} className=" w-[320px] overflow-hidden flex  relative group">
+            <Image 
+                className={className.image}
+                src={productDetails?.imagePath}
+                alt='product image'
+                width={320}
+                height={370}
+            />
+       </Link>
         <div className={className.heart}>
             <AddToListBtn product={productDetails} />
         </div>
         <div className="p-3">
-            <div className="pb-2">
+            <h2 className="text-xl font-bold text-teal-950 text-center ">
+                {getCurrency(productDetails?.priceInHalala)}
+            </h2>
+            <div className="pb-2 text-center ">
                 <Link
-                    href={`/product/${productDetails?.id}`}
+                    href={linkHref}
                     className="text-lg font-bold text-teal-900 hover:text-teal-900"
                     >{productDetails?.name}
                 </Link>
             </div>
-            <ColorOptions 
-                product={productDetails}
-                setProduct={setProductDetails}
+            <div className="flex justify-center items-center gap-3 flex-col mb-2">
+                <ColorOptions 
+                    product={productDetails}
+                    setProduct={setProductDetails}
+                    />
+                <SizesOptions
+                    product={productDetails}
+                    setProduct={setProductDetails}
                 />
-            <sizesOptions
-                product={productDetails}
-                setProduct={setProductDetails}
-             />
-            <h2 className="text-xl font-bold text-teal-950 text-center ">
-                {getCurrency(productDetails?.priceInHalala)}
-            </h2>
-            {/* <div className=" ">
+            </div>
+            <div className="">
               <AddToCart 
-                product={product} 
-                selectedColor={selectedColor}
-                selectedSize={selectedSize} 
+                product={productDetails}  
                 />
-            </div> */}
+            </div>
         </div>
     </div>
   )
