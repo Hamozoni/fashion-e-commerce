6517c45fdd391da4d149aa126ea640dcfe3879a4
Mohamed Yahia <m.yahya199290@gmail.com>
2024-07-19 10:25:10 +0300

    modified:   app/cart/_components/CartItemsCard.jsx

--- app/cart/_components/CartItemsCard.jsx
+++ app/cart/_components/CartItemsCard.jsx
@@ -21,19 +21,10 @@ const CartItemsCard = ({product,isCheckout = false})=> {
         query: {color,colorName,size,imagePath,priceInHalala}
      };
 
-    const className = {
-        tableBodyRow :'p-3 border border-gray-100'
-    }
 
   return (
     <div className="p-3 mb-8 border-l-2 border-l-teal-400 border-t-2 border-t-teal-400 rounded-md shadow-md border border-gray-100 w-full">
-         <Link href={linkHref}>
-            <h4 
-                className="text-md sm:text-xl font-bold text-teal-950 text-center hover:text-teal-800"
-                >{name}
-            </h4>
-       </Link>
-        <div className={`${isCheckout ? '':'flex'} gap-3 items-center`}>
+        <div className={`${isCheckout ? '':'flex'} gap-3`}>
             {
                 isCheckout ? null :
                 <Link 
@@ -49,48 +40,46 @@ const CartItemsCard = ({product,isCheckout = false})=> {
                 </Link>
             }
             <section>
-                <div className="sm:flex justify-between gap-3">
-                    <table className="rounded-md overflow-hidden mx-auto sm:mx-0">
-
-                        <thead className="pb-3">
-                            <tr className="text-teal-950 text-md sm:text-lg font-bold">
-                                <th className={className?.tableBodyRow}>color </th>
-                                <th className={className?.tableBodyRow}>size</th>
-                                <th className={className?.tableBodyRow}>price</th>
-                                <th className={className?.tableBodyRow}>total</th>
-
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr className="text-teal-900 text-sm sm:text-md font-bold">
-                                <td className={className?.tableBodyRow}> 
-                                    <p 
-                                        style={{backgroundColor: color}}
-                                        className="w-[35px] h-[10px] rounded-full"
-                                        >
-                                    </p> 
-                                </td>
-                                <td className={className?.tableBodyRow}>{size}</td>
-                                <td className={className?.tableBodyRow}>{getCurrency(priceInHalala)}</td>
-                                <td className={className?.tableBodyRow}>{getCurrency(priceInHalala * quantity)}</td>
-
-                            </tr>
-                        </tbody>
-                    </table>
-                    <div className="flex sm:flex-col justify-between pt-3 lg-pt-0">
+                <Link href={linkHref}>
+                        <h4 
+                            className="text-lg sm:text-xl font-bold text-teal-950 hover:text-teal-800"
+                            >{name}
+                        </h4>
+                </Link>
+                <div className="flex justify-between items-center mt-3">
+                    <div className="">
+                        <div className="flex gap-3 items-center lg-pt-0">
+                            <h5 className="text-lg text-teal-900">
+                                {getCurrency(priceInHalala)} × {quantity}
+                            </h5>
+                        </div>
+                        <div className="">
+                            <h5 className="text-lg text-teal-900">
+                                color:  <small>{colorName}</small> 
+                            </h5>
+                            <h5 className="text-lg text-teal-900">
+                                size:  <small>{size}</small>
+                            </h5>
+                        </div>
+                    </div>
+                    <div className="flex flex-col justify-between gap-2">
                         <QuantityBtn 
                             id={id} 
                             color={color} 
                             size={size} 
                             />
-                        <div className="max-w-24">
-                            <ButtonWithIcon 
-                                text='reove'
-                                type='delete'
-                                Icon={MdOutlineDeleteOutline}
-                                onClick={()=> dispatch(removeItemFromCart({id,color,size}))}
-                                />
-                        </div>
+                            {
+                                quantity > 1 ?
+                                <div className="">
+                                    <ButtonWithIcon 
+                                        text='' 
+                                        Icon={MdOutlineDeleteOutline} 
+                                        type='delete'
+                                        onClick={()=> dispatch(removeItemFromCart(id))}
+                                    />
+                                </div>
+                                : null
+                            }
                     </div>
                 </div>
             </section>
