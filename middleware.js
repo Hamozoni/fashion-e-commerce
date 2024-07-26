957f4a0b76bb1757afb43f948b4cb19d3067d39a
Mohamed Yahia <m.yahya199290@gmail.com>
2024-07-26 13:11:06 +0300

    modified:   middleware.js

--- middleware.js
+++ middleware.js
@@ -1,16 +1,16 @@
-import NextAuth from "next-auth";
-import authConfig from "./auth.config";
+// import NextAuth from "next-auth";
+// import authConfig from "./auth.config";
 
-import { getSession } from "next-auth/react";
+// import { getSession } from "next-auth/react";
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 
 
-const protectedRoutes = [ '/checkout','api/products/new','/admin/products/new']
- 
+
 export default  async function middleware (req) {
   // req.auth
+  const protectedRoutes = [ '/checkout','/api/products/new','/admin/products/new']
 
   const {pathname} = req.nextUrl;
 
@@ -35,7 +35,7 @@ export default  async function middleware (req) {
  
 // Optionally, don't invoke Middleware on some paths
 export const config = {
-  matcher: [ '/checkout','api/products/new'],
+  matcher: [ '/checkout','/api/products/new','/admin/products/new']
 }
 
 // /((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)
