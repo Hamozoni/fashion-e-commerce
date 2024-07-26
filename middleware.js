a7a6ad60bd54897342bdb6af864c371d7c79dc95
Mohamed Yahia <m.yahya199290@gmail.com>
2024-07-26 13:09:05 +0300

    modified:   middleware.js

--- middleware.js
+++ middleware.js
@@ -1,48 +1,37 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 
-import {poblicRoutes,authRoutes,DEFAULT_LOGIN_REDIRECT} from "./routes";
 import { getSession } from "next-auth/react";
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
 
 
-const { auth } = NextAuth(authConfig);
 
-const protectedRoutes = [ '/checkout','api/products/new']
+const protectedRoutes = [ '/checkout','api/products/new','/admin/products/new']
  
-export default  async function auth (req) {
+export default  async function middleware (req) {
   // req.auth
-  
-  const isLogined = !!req.auth;
+
   const {pathname} = req.nextUrl;
 
 
-  if(protectedRoutes.includes(pathname)){
+  if(protectedRoutes.some(route=> pathname.startsWith(route))){
 
-    const session = await getSession({req});
+    const token = await getToken({req});
 
-      if(!session){
-        return Response.redirect(new URL('/auth/login',req.nextUrl))
+      if(!token){
+
+        const url = req.nextUrl.clone();
+        url.pathname = '/auth/login';
+        url.searchParams.set('callbackUrl',req.nextUrl.href);
+        return NextResponse.redirect(url)
       }
-      return null
 
   }
 
+     return NextResponse.next()
 
-
-  if(!isLogined) {
-
-    let callback = pathname;
-    if(req.nextUrl.search){
-      callback += req.nextUrl.search
-    }
-
-    const encodedCallbackUrl = encodeURIComponent(callback)
-    return Response.redirect(new URL(`/auth/login?callback=${encodedCallbackUrl}`,req.nextUrl))
-  }
-
-  return null
-
-})
+}
  
 // Optionally, don't invoke Middleware on some paths
 export const config = {
