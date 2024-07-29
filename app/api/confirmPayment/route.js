17e3ee401cc58aaf070d731a9cff4fe37700bcf8
Mohamed Yahia <m.yahya199290@gmail.com>
2024-07-29 13:07:22 +0300

    modified:   app/api/confirmPayment/route.js

--- app/api/confirmPayment/route.js
+++ app/api/confirmPayment/route.js
@@ -1,5 +1,6 @@
 import Stripe from "stripe";
 import {db} from '../../../lip/db';
+import { NextResponse } from "next/server";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
@@ -19,16 +20,37 @@ export default async function POST (req) {
             totalPaidInCent
         } = await req.json();
 
-        const paymentIntent = await stripe.paymentIntents.retrieve(paymentId);
-
+        const {id,status} = await stripe.paymentIntents.retrieve(paymentId);
+        status.toUpperCase();
         const order = await db.order.create({
             data: {
-                
+                paymentId: id,
+                deliveryFree,
+                userId,
+                totalPaidInCent,
+                totalProductsQuantity,
+                products : {
+                    create : products
+                },
+                payment : {
+                    create : {
+                        id,
+                        amountInCent: totalPaidInCent,
+                        status,
+                        userId
+                    }
+                }
+
             }
-        })
+        });
+
+
+        return NextResponse.json(order,{status: 200});
 
     }
     catch (error) {
 
+        console.log(error)
+        return NextResponse.json("something went wrong",{status: 500});
     }
 }
