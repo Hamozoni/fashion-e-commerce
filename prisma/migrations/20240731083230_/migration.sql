59d49e711edd0249658ce94a89bcd1ad3dceac75
Mohamed Yahia <m.yahya199290@gmail.com>
2024-07-31 14:30:58 +0300

    deleted:    prisma/migrations/20240731050647_/migration.sql         deleted:    prisma/migrations/20240731061137_/migration.sql         deleted:    prisma/migrations/20240731083230_/migration.sql         deleted:    prisma/migrations/migration_lock.toml

--- prisma/migrations/20240731083230_/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE `orderspayments` DROP FOREIGN KEY `ordersPayments_orderId_fkey`;
-
--- AddForeignKey
-ALTER TABLE `ordersPayments` ADD CONSTRAINT `ordersPayments_orderId_fkey` FOREIGN KEY (`orderId`) REFERENCES `customerOrders`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
