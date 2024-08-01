97c4f95980744e299d5b65262c486fd235638ea0
Mohamed Yahia <m.yahya199290@gmail.com>
2024-08-01 08:57:18 +0300

    app/(protected)/orders/ordersContainer.jsx

---
+++ app/(protected)/orders/ordersContainer.jsx
@@ -0,0 +1,42 @@
+"use client";
+
+
+
+import { useContext, useEffect, useState } from "react";
+import {AppContext} from "../../contextProvider";
+import {fetchData} from "../../../lip/fetchData";
+import {Loading} from "../../loading";
+
+export const OrdersContainer = ()=> {
+
+    const [orders,setOrders] = useState([]);
+    const [isLoading,setIsLoading] = useState(false);
+    const [error,setError] = useState(null);
+
+    const {currentUser: {id}} = useContext(AppContext);
+
+
+    useEffect(()=> {
+        setIsLoading(true);
+
+        fetchData(`orders?userId=${id}`)
+        .then((data)=> {
+
+            console.log(data)
+        })
+        .catch((error)=> {
+            console.log(error)
+        })
+        .finally(()=> {
+            setIsLoading(false)
+        })
+    },[id]);
+
+    if(isLoading) return <Loading />
+
+    return (
+        <div className="">
+             
+        </div>
+    )
+}
--- app/(protected)/orders/page.jsx
+++ app/(protected)/orders/page.jsx
@@ -1,50 +1,14 @@
-"use client";
-import React, { useContext, useEffect, useState } from 'react'
-import { fetchData } from '../../../lip/fetchData';
-import { AppContext } from '../../contextProvider';
-
-import Loading from "../../loading";
-import { useRouter } from 'next/navigation';
+import { OrdersContainer } from "./ordersContainer"
 
 function orderPage() {
 
-  const [orders,setOrders] = useState([]);
-  const [isLoading,setIsLoading] = useState(false);
-  const [error,setError] = useState(null);
-
-  const {currentUser: {id}} = useContext(AppContext);
-
-  const router = useRouter()
-
-  if(!id) {
-     router.push('/auth/login');
-
-     return;
-  }
-
-
-  useEffect(()=> {
-
-    setIsLoading(true);
-
-      fetchData(`orders?userId=${id}`)
-      .then((data)=> {
-        console.log(data)
-      })
-      .catch((error)=> {
-        console.log(error)
-      })
-      .finally(()=> {
-           setIsLoading(false)
-      })
-
-
-  },[id]);
-
-  if(isLoading) return <Loading />
-
   return (
-    <div>orderPage</div>
+    <div>
+      <header>
+          <h4>your orders</h4>
+      </header>
+      <OrdersContainer />
+    </div>
   )
 }
 
