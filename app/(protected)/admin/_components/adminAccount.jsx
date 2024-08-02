58653e618cec846d79df795b8c2e7a2a00351516
Mohamed Yahia <m.yahya199290@gmail.com>
2024-08-02 17:25:32 +0300

    modified:   app/(protected)/admin/_components/adminAccount.jsx         modified:   app/(protected)/admin/_components/header/Header.jsx

---
+++ app/(protected)/admin/_components/adminAccount.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useContext } from "react";
+import {AppContext} from "../../../contextProvider";
+
+export const AdminAccount = ()=> {
+
+    const {currentUser} = useContext(AppContext)
+    return (
+        <div className="">
+            
+        </div>
+    )
+}
--- app/(protected)/admin/_components/header/Header.jsx
+++ app/(protected)/admin/_components/header/Header.jsx
@@ -4,34 +4,42 @@ import Link from "next/link";
 import { LuLayoutDashboard } from "react-icons/lu";
 import { GoPeople } from "react-icons/go";
 import { MdOutlineLocalOffer,MdOutlinePropane } from "react-icons/md";
+import { IoMdAdd } from "react-icons/io";
+import { ThemeModel } from "../../../../../ui/models/themeModel";
 
 const Header = () => {
 
   const className = {
-     link: 'capitalize text-md font-medium text-slate-200 hover:text-black-500 flex items-center gap-2 w-fit min-w-fit',
+     link: 'capitalize m-0 py-3 px-4 text-md font-medium text-slate-200 hover:bg-gray-600 hover:text-black-500 flex items-center gap-2 w-fit min-w-[200px]',
   }
   return (
-    <header className=" bg-gray-600 p-6 lg:px-10">
+    <header className=" bg-gray-800">
+      <h4 className="p-3">
         <Link href={`/`} className="text-2xl font-bold uppercase text-slate-100">
-          system Admin
+             system
         </Link>
-        <nav className="flex flex-col gap-5">
+
+      </h4>
+        <nav className="flex flex-col gap-5 mt-3 sticky top-0 left-0 h-screen">
             <Link 
                  href={`/admin/dashboard`} className={className.link}>
-                <LuLayoutDashboard />  dashboard
+                <LuLayoutDashboard size={24}/>  dashboard
             </Link>
             <Link href={`/admin/products`} className={className.link} >
-               <MdOutlinePropane /> products
+               <MdOutlinePropane size={24} /> products
             </Link>
             <Link href={`/admin/products/new`} className={className.link} >
-               <MdOutlinePropane /> add new products
+               <IoMdAdd size={24} /> add new product
             </Link>
             <Link href={`/admin/customers`} className={className.link} >
-                <GoPeople />  customers
+                <GoPeople size={24}/>  customers
               </Link>
             <Link href={`/admin/customers`} className={className.link} >
-               <MdOutlineLocalOffer /> sales
-              </Link>
+               <MdOutlineLocalOffer size={24} /> sales
+            </Link>
+            <div >
+              <ThemeModel />
+            </div>
         </nav>
     </header>
   )
