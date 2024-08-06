aed3119541e8e1dc43c38d620bbb4b5db20dfe7e
Mohamed Yahia <m.yahya199290@gmail.com>
2024-08-06 20:53:54 +0300

    modified:   app/(protected)/admin/products/_components/navbar.jsx         modified:   app/(protected)/admin/products/page.jsx

--- app/(protected)/admin/products/_components/navbar.jsx
+++ app/(protected)/admin/products/_components/navbar.jsx
@@ -3,6 +3,8 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import {fetchData} from "../../../../../lip/fetchData";
+import { Loading } from "../../../../../ui/models/Loading";
+import { Error } from "../../../../../ui/components/error";
 
 export const  Navbar = ()=> {
 
@@ -11,20 +13,43 @@ export const  Navbar = ()=> {
     const [error,setError] = useState(null);
 
 
-
-    useEffect(()=> {
+    const handleFetch = ()=> {
         setIsLoading(true);
         fetchData('admin/products/sectionsQuantity')
         .then((data)=> {
+            setSectionData(data)
             console.log(data)
         })
+        .catch((error)=> {
+            setError(error)
+        })
+        .finally(()=> {
+            setIsLoading(false)
+        })
+    }
+    useEffect(()=> {
+        handleFetch()
     },[]);
+
+    if(isLoading) return <Loading />
+    if(error) return <Error onClick={handleFetch} />
 
     return (
         <header>
             <nav>
-                <ul>
-                    <li></li>
+                <ul className="flex items-center gap-3 overflow-x-auto">
+                   {
+                      sectionsData?.map(({name,items,quantity})=> (
+                        <li 
+                            className="p-3 rounded-m bg-gray-50 border-gray-200 text-teal-950 capitalize text-[16px] min-w-fit font-bold"
+                            key={name}>
+                                <h5>{{name}}</h5>
+                                <h6>{items} items</h6>
+                                <h6>{quantity} quantity</h6>
+                                 
+                            </li>
+                      ))
+                   }
                 </ul>
             </nav>
         </header>
--- app/(protected)/admin/products/page.jsx
+++ app/(protected)/admin/products/page.jsx
@@ -1,6 +1,4 @@
-"use client"
-import Link from "next/link";
-import { IoAdd } from "react-icons/io5";
+
 import {Navbar} from "./_components/navbar"
 
 const Products = () => {
