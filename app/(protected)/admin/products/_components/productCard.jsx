1644c9b5d9086d5071ca8dd34584ed1dd0cfbacd
Mohamed Yahia <m.yahya199290@gmail.com>
2024-08-09 20:05:06 +0300

    modified:   app/(protected)/admin/products/_components/productCard.jsx

--- app/(protected)/admin/products/_components/productCard.jsx
+++ app/(protected)/admin/products/_components/productCard.jsx
@@ -4,8 +4,8 @@ import {getCurrency} from "../../../../../lip/getCurrency";
 export const ProductCard = ({product})=> {
     const {imagePath,brand,name,colors,sizes} = product
     return (
-        <div className="">
-            <div className="">
+        <div className="flex items-center gap-2 my-5 p-3 bg-teal-50 rounded-md border border-teal-100 capitalize">
+            <div className="min-w-[80px]">
                 <Image  
                     src={imagePath} 
                     width={80} height={80} 
@@ -14,29 +14,32 @@ export const ProductCard = ({product})=> {
             </div>
             <div className="">
                 <div className="">
-                    <h4>{brand}</h4>
-                    <h3>{name}</h3>
+                    <h4 className="text-xs font-bold text-gray-600">{brand}</h4>
+                    <h3 className="line-clamp-1 text-sm font-bold text-teal-950 mb-2">{name}</h3>
                 </div>
-                <div className="">
+                <div className="flex items-center gap-4 flex-wrap">
                     {
                         colors?.map(({color,colorName,priceInHalala})=> (
-                            <div key={color} className="" >
+                            <div key={color} className="flex items-center gap-2 p-2 rounded-md bg-white" >
                                 <div className="">
                                     <h5>{getCurrency(priceInHalala)}</h5>
-                                    <h6>{colorName}</h6>
                                     <div 
                                         style={{backgroundColor: color}} 
-                                        className="w-[40ps] h-[40px] rounded-md border border-teal-100"
+                                        className=" h-[30px] rounded-md border border-teal-100"
                                         >
 
                                     </div>
                                 </div>
+                                <div className="p-2 rounded-md bg-teal-50">
+                                    <h6>sizes: </h6>
+                                    <h6>quantities: </h6>
+                                </div>
                                 {
-                                    sizes?.map(({colorName : color,name,stackQuantity})=> (
+                                    sizes?.map(({colorName : color,shortName,stackQuantity})=> (
                                         colorName === color ?
-                                        <div className="">
-                                            <h6>size: {name}</h6>
-                                            <p>stack Quantity: {stackQuantity}</p>
+                                        <div className="p-2 rounded-md bg-teal-50">
+                                            <h6>{shortName}</h6>
+                                            <p>{stackQuantity}</p>
                                         </div>
                                         : null
                                     ))
