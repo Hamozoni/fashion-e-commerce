8aab20a276fb0fd8835f2f7d13d00e468aab6a99
Mohamed Yahia <m.yahya199290@gmail.com>
2024-08-09 16:17:21 +0300

    app/api/admin/products/route.js

--- app/(protected)/admin/products/_components/navbar.jsx
+++ app/(protected)/admin/products/_components/navbar.jsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 export const Navbar = ()=> {
     const [categoryName,setCategoryName] = useState('all');
     const [subcategoryName,setSubcategoryName] = useState('all');
-    const [subCategory,setSubCategory] = useState(categoriesData[0]?.sub);
+    const [subCategory,setSubCategory] = useState('all');
     const [isSubcategory,setIsSubcategory] = useState(false)
 
     const handleCategory = (category)=> {
