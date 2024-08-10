502aa1e854e93e8ab009000ed4952274949a58d9
Mohamed Yahia <m.yahya199290@gmail.com>
2024-08-10 08:15:58 +0300

    modified:   ui/models/Overlay.jsx

--- ui/models/Overlay.jsx
+++ ui/models/Overlay.jsx
@@ -2,7 +2,7 @@
 export function Overlay({onClick}) {
   return (
     <div 
-        className="fixed top-0 left-0 w-full min-w-full h-screen min-h-screen bg-black dark:bg-white cursor-pointer opacity-30 z-[99]"
+        className="fixed top-0 left-0 w-full min-w-full h-screen min-h-screen bg-black dark:bg-white cursor-pointer opacity-30 z-40"
         onClick={onClick}
         >
     </div>
