8d472ad920e10ffe677e2f61315f8169e954b4c8
Mohamed Yahia <m.yahya199290@gmail.com>
2024-08-10 09:20:13 +0300

    modified:   app/(protected)/admin/products/_components/productCard.jsx

--- app/(protected)/admin/products/_components/productCard.jsx
+++ app/(protected)/admin/products/_components/productCard.jsx
@@ -5,11 +5,11 @@ export const ProductCard = ({product})=> {
     const {imagePath,brand,name,colors,sizes,id} = product;
 
     const className = {
-        smStitle : 'text-xs font-bold text-gray-600'
+        smStitle : 'text-xs font-bold text-gray-600 dark:text-gray-300'
     }
     return (
-        <div className="gap-2 my-5 p-3 bg-teal-50 rounded-md border border-teal-100 capitalize">
-            <div className="flex  gap-3 mb-2 border border-white">
+        <div className="gap-2 my-5 p-3 bg-teal-50 dark:bg-stone-900 rounded-md border border-teal-100 dark:border-stone-800 capitalize">
+            <div className="flex  gap-3 mb-2 border border-white dark:border-stone-600 rounded-md">
                 <div className="w-[60px] h-[60px] min-w-[60px] rounded-md bg-white overflow-hidden">
                     <Image 
                        className="min-w-[60px] max-w-[60px] min-h-[60px] max-h-[60px] object-contain" 
@@ -21,7 +21,7 @@ export const ProductCard = ({product})=> {
                 </div>
                 <div className="">
                     <h4 className={className.smStitle}>{brand}</h4>
-                    <h3 className="line-clamp-1 text-sm font-bold text-teal-950 mb-2">{name}</h3>
+                    <h3 className="line-clamp-1 text-sm font-bold text-teal-950 dark:text-teal-50 mb-2">{name}</h3>
                     <h6 className={className.smStitle}>ID : <small>{id}</small></h6>
                 </div>
             </div>
@@ -29,7 +29,7 @@ export const ProductCard = ({product})=> {
                 <div className="flex items-center gap-4 flex-wrap">
                     {
                         colors?.map(({color,colorName,priceInHalala})=> (
-                            <div key={color} className="flex items-center gap-2 p-[2px] rounded-md bg-white flex-grow" >
+                            <div key={color} className="flex items-center gap-2 p-[2px] rounded-md bg-white dark:bg-stone-700 flex-grow" >
                                 <div className="">
                                     <h5 className={className.smStitle}>{getCurrency(priceInHalala)}</h5>
                                     <div 
@@ -39,7 +39,7 @@ export const ProductCard = ({product})=> {
 
                                     </div>
                                 </div>
-                                <div className="flex gap-2 items-center p-2 rounded-md bg-teal-50 flex-grow">
+                                <div className="flex gap-2 items-center p-2 rounded-md bg-gray-50 dark:bg-stone-600 flex-grow">
                                     <div className="">
                                         <h6 className={className.smStitle}>sizes: </h6>
                                         <h6 className={className.smStitle}>quantities: </h6>
@@ -47,7 +47,7 @@ export const ProductCard = ({product})=> {
                                     {
                                         sizes?.map(({colorName : color,shortName,stackQuantity})=> (
                                             colorName === color ?
-                                            <div className="text-center bg-white p-2 rounded-md">
+                                            <div className="text-center bg-white dark:bg-stone-700 p-2 rounded-md">
                                                 <h6 className={className.smStitle}>{shortName}</h6>
                                                 <p className={className.smStitle}>{stackQuantity}</p>
                                             </div>
