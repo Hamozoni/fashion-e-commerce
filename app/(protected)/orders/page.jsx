e17bab586363b661fabd03d55ac6a4b2131e771b
Mohamed Yahia <m.yahya199290@gmail.com>
2024-08-15 12:42:19 +0300

    app/(protected)/orders/statusNavbar.js

--- app/(protected)/orders/page.jsx
+++ app/(protected)/orders/page.jsx
@@ -1,33 +1,16 @@
 
 import { OrdersContainer } from "./ordersContainer";
-import { StatusLi } from "./statusLi";
+import { StatusNavbar } from "./statusNavbar";
 
-async function orderPage({searchParams}) {
-
-const  status = searchParams.status;
-
-const orderStatus =  ["pending","prossing","completed","canceled"];
-
-const className = {
-  li: 'p-3 rounded-md text-sm font-bold text-teal-950 dark:text-teal-50 bg-gray-100 dark:bg-stone-900 cursor-pointer'
-}
+async function orderPage() {
 
   return (
     <div className="p-3 md:px-8">
-      <header className="flex items-center justify-between flex-wrap capitalize mb-8">
+      <header className="flex items-center justify-between flex-wrap gap-3 capitalize mb-8">
           <h4 className="text-teal-950 dark:text-teal-50 font-bold text-xl"
             >your orders
           </h4>
-          <nav>
-              <ul className="flex items-center justify-center gap-3">
-                <li className={className?.li}>all</li>
-                {
-                  orderStatus?.map((st)=> (
-                    <StatusLi key={st} st={st} />
-                  ))
-                }
-              </ul>
-          </nav>
+          <StatusNavbar />
       </header>
       <OrdersContainer />
     </div>
---
+++ app/(protected)/orders/statusNavbar.jsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+
+export const StatusNavbar = ({st})=> {
+
+    const searchParams = useSearchParams();
+    const status = searchParams.get('status');
+    const router = useRouter()
+
+    const orderStatus =  ["pending","prossing","completed","canceled"];
+
+    const className = {
+        li: 'text-teal-950 dark:text-teal-50 bg-gray-100 dark:bg-stone-900'
+      }
+
+    return (
+
+    <nav className="px-3 py-1 bg-gray-100 dark:bg-stone-950 overflow-x-auto rounded-md">
+        <ul className="flex items-center gap-3 ">
+          <li 
+              onClick={()=> router.push(`/orders`)} 
+              className={`${!status ? 'bg-teal-400 text-teal-950' : className?.li} px-4 py-2 rounded-md text-sm font-bold cursor-pointer `}
+          >all</li>
+          {
+            orderStatus?.map((st)=> (
+                <li
+                    onClick={()=> router.push(`/orders?status=${st}`)} 
+                    className={`${status === st ? 'bg-teal-400 text-teal-950' : className?.li} px-4 py-2 rounded-md text-sm font-bold cursor-pointer `}
+                    key={st}>
+                    {st}
+                 </li>
+            ))
+          }
+        </ul>
+    </nav>
+    )
+}
