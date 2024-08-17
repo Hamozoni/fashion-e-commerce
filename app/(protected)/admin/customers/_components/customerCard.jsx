12955c718ba60436b3cdf59f29ab8b7384fb3601
Mohamed Yahia <m.yahya199290@gmail.com>
2024-08-17 18:52:10 +0300

    modified:   app/(protected)/admin/customers/_components/customerCard.jsx

--- app/(protected)/admin/customers/_components/customerCard.jsx
+++ app/(protected)/admin/customers/_components/customerCard.jsx
@@ -11,46 +11,51 @@ export const CustomerCard = ({customer})=> {
     const totalPaid = orders?.length > 0 ? orders.reduce((total,curr)=> total + curr.totalPaidInCent,0) : 0;
     const avgRating = reviews?.length > 0 ? reviews.reduce((total,curr)=> total + curr.rating,0) / reviews?.length : 0;
 
-
+    const className = {
+        tHead: 'bg-white text-sm font-bold text-gray-400 p-2 capitalize',
+        bTitle: 'text-[16px] font-bold capitalize text-teal-950',
+        bSTilte: 'text-xs font-bold text-teal-900',
+        bTTitle:'text-sm font-bold text-teal-900 capitalize flex items-center gap-3'
+    }
     return (
-        <div className="">
-            <section className="">
-                <h5>info: </h5>
-                <div className="">
+        <div className="flex p-3 rounded-md bg-gray-50 border border-gray-100 mb-3">
+            <section className="flex-1">
+                <h5 className={className.tHead}>info: </h5>
+                <div className="flex items-center gap-3 p-2">
                     <div className="">
                         {
                             image ?
                             <Image src={image} width={70} height={70} alt='user' />
-                            : <FaRegUser size={70} />
+                            : <FaRegUser size={50} />
                         }
                     </div>
                     <div className="">
-                        <h6>{name}</h6>
-                        <p>{email}</p>
+                        <h6 className={className.bTitle}>{name}</h6>
+                        <p className={className.bSTilte}>{email}</p>
                     </div>
                 </div>
             </section>
-            <section className="">
-                <h5>orders: </h5>
-                <div className="">
-                    <div className="">
+            <section className="flex-1">
+                <h5 className={className.tHead}>orders: </h5>
+                <div className="p-2">
+                    <div className={className.bTTitle}>
                         <h5>total orders: </h5>
                         <p>{orders?.length}</p>
                     </div>
-                    <div className="">
+                    <div className={className.bTTitle}>
                         <h5>total paid: </h5>
                         <p>{getCurrency(totalPaid)}</p>
                     </div>
                 </div>
             </section>
-            <section className="">
-                <h5>reviews: </h5>
-                <div className="">
-                    <div className="">
+            <section className="flex-1">
+                <h5 className={className.tHead}>reviews: </h5>
+                <div className="p-2">
+                    <div className={className.bTTitle}>
                         <h5>total reviews: </h5>
                         <p>{reviews?.length}</p>
                     </div>
-                    <div className="">
+                    <div className={className.bTTitle}>
                         <h5>rating: </h5>
                         <RatingStars rating={avgRating} />
                     </div>
