53dcd0ed2cc1ec7968bf02c112ff9b6370124845
Mohamed Yahia <m.yahya199290@gmail.com>
2024-08-17 14:45:14 +0300

    modified:   app/(protected)/admin/customers/_components/header.jsx         modified:   app/api/admin/customers/route.js

--- app/(protected)/admin/customers/_components/header.jsx
+++ app/(protected)/admin/customers/_components/header.jsx
@@ -1,10 +1,36 @@
-
+import { FaUsers } from "react-icons/fa";
+import { MdOutlineVerifiedUser } from "react-icons/md";
+import { FaUserSlash } from "react-icons/fa6";
+import { FaUserCheck } from "react-icons/fa6";
 export const Header = ({data})=> {
-    
+
     const {allCount,adminCount,verifiedCount,unverifiedCount} = data;
+    const headerData = [
+        {name: 'all customers',count: allCount,Icon: FaUsers},
+        {name: 'verified customers',count: verifiedCount,Icon:FaUserCheck },
+        {name: 'unverified customers',count: unverifiedCount,Icon:  FaUserSlash},
+        {name: 'admins',count: adminCount,Icon: MdOutlineVerifiedUser},
+    ]
+
+
+    console.log({allCount,adminCount,verifiedCount,unverifiedCount})
 
     return (
         <header>
+            <ul className="flex items-center gap-5">
+                {
+                    headerData?.map(({name,count,Icon})=> (
+                        <li key={name}>
+                            <div className="flex items-center gap-3">
+                                <Icon />
+                                <h5>{count}</h5>
+                            </div>
+                            <h6>{name}</h6>
+                        </li>
+
+                    ))
+                }
+            </ul>
         </header>
     )
 }
--- app/api/admin/customers/route.js
+++ app/api/admin/customers/route.js
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 
 export async function GET (req){
 
-    const searchParams = new URL(req.url);
+    const {searchParams} = new URL(req.url);
 
     const page = searchParams.get('page') || 1
 
