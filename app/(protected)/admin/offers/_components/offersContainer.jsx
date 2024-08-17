551df953820c0b79e895fee1b7cf09e92bdf0e75
Mohamed Yahia <m.yahya199290@gmail.com>
2024-08-17 08:12:23 +0300

    modified:   app/(protected)/admin/offers/_components/offersContainer.jsx

--- app/(protected)/admin/offers/_components/offersContainer.jsx
+++ app/(protected)/admin/offers/_components/offersContainer.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Navbar } from "../../products/_components/navbar"
 import { OfferProductCard } from "./offerProductCard";
 
@@ -20,6 +20,7 @@ export const OffersContainer = ()=> {
     const [products,setProducts] = useState([]);
     const [count,setCount] = useState(0);
     const [isLoading,setIsLoading] = useState(false);
+    const [isValid,setIsValid] = useState(false);
     const [isErrror,setIsError] = useState(null);
     const [page,setPage] = useState(1);
 
@@ -41,8 +42,23 @@ export const OffersContainer = ()=> {
         })
     };
 
+    useEffect(()=> {
+        const nowDate = Date.now();
+        setProducts(prev=> {
+            let filteredProducts = [] 
+            if(isValid){
+                filteredProducts = prev.filter((e)=>  Date.parse(e.offerExpiresAt) > nowDate);
+            }else {
+                filteredProducts = prev.filter((e)=>  Date.parse(e.offerExpiresAt) < nowDate);
+            }
+            return [...filteredProducts]
+        })
+    },[isValid]);
+
     const className = {
-        title : 'flex items-center gap-2 text-lg font-bold text-gray-400 mb-3'
+        title : 'flex items-center gap-2 text-lg font-bold text-gray-400',
+        validBtn: `${isValid ? 'text-teal-300 border-b border-b-teal-300': 'text-teal-950 dark:text-teal-50 ' } font-bold capitalize text-sm`,
+        expiresBtn: `${isValid ? 'text-teal-950 dark:text-teal-50': ' text-teal-300 border-b border-b-teal-300' } font-bold capitalize text-sm`,
     }
 
 
@@ -54,9 +70,23 @@ export const OffersContainer = ()=> {
                setPage={setPage}
             />
             <div className="">
-                <h5 className={className.title}>
-                    <TbAdjustmentsSearch size={24} /> all results {count}
-                </h5>
+                <section className="flex items-center justify-between mb-3">
+                    <h5 className={className.title}>
+                        <TbAdjustmentsSearch size={24} /> all results {count}
+                    </h5>
+                    <nav className="flex items-center gap-5">
+                        <button 
+                            onClick={()=> setIsValid(true)}
+                            className={className.validBtn}>
+                                valid
+                            </button>
+                        <button
+                            onClick={()=> setIsValid(false)} 
+                            className={className.expiresBtn}>
+                                expired
+                            </button>
+                    </nav>
+                </section>
                 <div className="">
                     {
                         products?.map(product=> (
