fb5605ec091e0cc14cd4c8e2a0ccf97c0136d588
Mohamed Yahia <m.yahya199290@gmail.com>
2024-08-17 09:27:40 +0300

    modified:   app/(protected)/admin/offers/_components/offersContainer.jsx

--- app/(protected)/admin/offers/_components/offersContainer.jsx
+++ app/(protected)/admin/offers/_components/offersContainer.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import {useState } from "react";
 import { Navbar } from "../../products/_components/navbar"
 import { OfferProductCard } from "./offerProductCard";
 
