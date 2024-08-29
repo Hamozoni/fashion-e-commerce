55a8472acaf44c99ac497dd9c771fb3d028744e1
Mohamed Yahia <m.yahya199290@gmail.com>
2024-08-29 20:55:44 +0300

    modified:   app/(protected)/admin/layout.js

--- app/(protected)/admin/layout.jsx
+++ app/(protected)/admin/layout.jsx
@@ -1,12 +1,20 @@
+import { auth } from "@/auth";
 import { Header } from "./_components/header";
 import {Navbar} from "./_components/navbar";
+import { redirect } from "next/navigation";
 
 export const metadata = {
     title: "admin",
     description: "admid page for hamozoni fashion store",
   };
   
-  export default function AdminLayout({ children }) {
+  export default async function AdminLayout({ children }) {
+
+    const session = await auth();
+
+    if(!session?.user) {
+      return redirect(`/auth/login?callback=admin`)
+    }
     return (
           <div className="md:flex items-start">
             <Navbar />
